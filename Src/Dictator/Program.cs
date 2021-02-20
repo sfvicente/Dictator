887d039525c15189c60f69db1c7b0515005cfe6f
Sergio Vicente <sfvicente@gmail.com>
2021-02-20 23:54:26 +0000

    Added scaffold for the console application project

---
+++ Src/Dictator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dictator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
