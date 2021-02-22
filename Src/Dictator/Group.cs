d82bd66030fa57f7e239e6bcc9eb0571438005c5
Sergio Vicente <sfvicente@gmail.com>
2021-02-22 23:03:21 +0000

    Added initial scaffold for the Group model to control popularity, status and strength of factions

---
+++ Src/Dictator/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Group
+{
+    public string Name { get; set; }
+    public int Popularity { get; set; }
+    public int Strength { get; set; }
+    public GroupStatus Status  { get; set; }
+
+    public Group()
+	{
+	}
+}
---
+++ Src/Dictator/GroupStatus.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public enum GroupStatus
+{
+    Default = 0,
+    Assassination = 1,
+    Revolution = 2
+}
