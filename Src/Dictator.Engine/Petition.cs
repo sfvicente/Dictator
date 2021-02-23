2eaf3ef868807707a3bff8f6ba14ad0e8c846e31
Sergio Vicente <sfvicente@gmail.com>
2021-02-23 22:20:24 +0000

    Added initial version of the Petition class

---
+++ Src/Dictator.Engine/Petition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Engine
+{
+    public class Petition
+    {
+        public int Cost { get; set; }
+        public bool IsMonthlyCost { get; set; }
+        public string GroupPopularityChanges { get; set; }
+        public string GroupStrenghtChanges { get; set; }
+
+        public string Text { get; set; }
+        public GroupType Requester { get; set; }
+
+        public bool HasBeenUsed { get; set; }
+    }
+}
