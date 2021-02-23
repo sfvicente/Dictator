5263ca1417d126cd6ddda834e6b6f7eb88c3b62a
Sergio Vicente <sfvicente@gmail.com>
2021-02-23 00:10:39 +0000

    Added mechanism for initialisation of the group data with expansion of required class structure

--- Src/Dictator.Engine/Group.cs
+++ Src/Dictator.Engine/Group.cs
@@ -4,14 +4,21 @@ namespace Dictator.Engine
 {
     public class Group
     {
-        public string DisplayName { get; set; }
-        public GroupType groupType { get; set; }
+        public GroupType Type { get; set; }
         public int Popularity { get; set; }
         public int Strength { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
         public GroupStatus Status { get; set; }
 
-        public Group()
+        public Group(GroupType groupType, int popularity, int strength, string name, string displayName)
         {
+            this.Type = groupType;
+            this.Popularity = popularity;
+            this.Strength = strength;
+            this.Name = name;
+            this.DisplayName = displayName;
+            this.Status = GroupStatus.Default;
         }
     }
 }
