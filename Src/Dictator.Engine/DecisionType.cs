dbe8720ede4891e8a294b4c1d05e499d0057d707
Sergio Vicente <sfvicente@gmail.com>
2021-02-23 22:29:43 +0000

    Added intial version of the presidential decision implementation

---
+++ Src/Dictator.Engine/DecisionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Engine
+{
+    public enum DecisionType
+    {
+        PleaseAGroup = 0,
+        PleaseAllGroups = 1,
+        ImproveYourChanges = 2,
+        RaiseSomeCash = 3,
+        StrengthenAGroup = 4
+    }
+}
