cf9c1ccd4fa7cbae3d29d7638c4bbe14079ed7e7
Sergio Vicente <sfvicente@gmail.com>
2021-02-27 22:29:03 +0000

    Added scaffold for the initial version of the news flash screen

---
+++ Src/Dictator.ConsoleInterface/NewsFlashScreen.cs
@@ -0,0 +1,17 @@
+﻿using Dictator.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.ConsoleInterface
+{
+    public class NewsFlashScreen
+    {
+        public void Draw()
+        {
+            ConsoleEx.Clear(ConsoleColor.Gray);
+            ConsoleEx.WriteAt(24, 10, "NEWSFLASH");
+            ConsoleEx.WriteAt(24, 4, "{newsStats.text}");
+        }
+    }
+}
