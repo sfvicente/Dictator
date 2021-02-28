902eef2a9ba5892dc959ee8654e4c39b30f3afd6
Sergio Vicente <sfvicente@gmail.com>
2021-02-28 01:48:04 +0000

    Complemented the introduction screen with colors and correct user input

--- Src/Dictator.ConsoleInterface/IntroScreen.cs
+++ Src/Dictator.ConsoleInterface/IntroScreen.cs
@@ -10,24 +10,24 @@ namespace Dictator.ConsoleInterface
         public void Show()
         {
             ConsoleEx.Clear(ConsoleColor.Black);
-            ConsoleEx.WriteAt(24, 4, "  DICTATOR                      ");
+            ConsoleEx.WriteAt(26, 4, "DICTATOR", ConsoleColor.White, ConsoleColor.Black);
             ConsoleEx.WriteAt(24, 8, "  Devised and Written by        ");
             ConsoleEx.WriteAt(24, 10, "  Don PRIESTLEY                 ");
 
             ConsoleEx.WriteAt(24, 14, "  Copyright  ");
-            Console.Write('D');
-            Console.Write('k');
-            Console.Write('T');
-            Console.Write('R');
-            Console.Write('O');
-            Console.Write('N');
-            Console.Write('I');
-            Console.Write('C');
-            Console.Write('S');
+            ConsoleEx.Write("D", ConsoleColor.White, ConsoleColor.Black);
+            ConsoleEx.Write("k", ConsoleColor.Yellow, ConsoleColor.Black);
+            ConsoleEx.Write("T", ConsoleColor.White, ConsoleColor.Black);
+            ConsoleEx.Write("R", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.Write("O", ConsoleColor.Cyan, ConsoleColor.Black);
+            ConsoleEx.Write("N", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.Write("I", ConsoleColor.Cyan, ConsoleColor.Black);
+            ConsoleEx.Write("C", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.Write("S", ConsoleColor.Cyan, ConsoleColor.Black);
             Console.Write("  1983");
 
             ConsoleEx.WriteAt(24, 18, "  Rewritten in C# by Sergio Vicente 2021  ");
-            Console.ReadKey();
+            Console.ReadKey(true);
         }
     }
 }
