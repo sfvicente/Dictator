0d7aa68fd260d78e435544e2255fca3cbedab775
Sergio Vicente <sfvicente@gmail.com>
2021-03-03 22:01:36 +0000

    Added scaffold for the end game screen

---
+++ Src/Dictator.ConsoleInterface/EndScreen.cs
@@ -0,0 +1,26 @@
+﻿using Dictator.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.ConsoleInterface
+{
+    public class EndScreen : IEndScreen
+    {
+        private readonly IAccount account;
+        private readonly IGovernmentStats governmentStats;
+        private readonly IGroupStats groupStats;
+
+        public EndScreen(IAccount account, IGovernmentStats governmentStats, IGroupStats groupStats)
+        {
+            this.account = account;
+            this.governmentStats = governmentStats;
+            this.groupStats = groupStats;
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Src/Dictator/Program.cs
+++ Src/Dictator/Program.cs
@@ -33,6 +33,7 @@ namespace Dictator
             .AddScoped<IMonthScreen, MonthScreen>()
             .AddScoped<IDecisionMainDialog, DecisionMainDialog>()
             .AddScoped<IDecisionSubDialog, DecisionSubDialog>()
+            .AddScoped<IEndScreen, EndScreen>()
             .AddScoped<IAccount, Account>()
             .AddScoped<IGovernmentStats, GovernmentStats>()
             .AddScoped<IGroupStats, GroupStats>()
