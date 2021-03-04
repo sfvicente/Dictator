0cd1ee02cf22a690561526a26b3715a1822f9c54
Sergio Vicente <sfvicente@gmail.com>
2021-03-04 23:03:43 +0000

    Moved the Game class into the main startup project as it doesn't belong in the UI project

---
+++ Src/Dictator/Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dictator.Core;
+
+namespace Dictator.ConsoleInterface
+{
+    public class Game
+    {
+        private IEngine engine;
+        private IUserInterface userInterface;
+
+        public Game(IEngine engine, IUserInterface userInterface)
+        {
+            this.engine = engine;
+            this.userInterface = userInterface;
+        }
+
+        public void Start()
+        {
+            userInterface.DisplayIntroScreen();
+            userInterface.DisplayTitleScreen();
+            userInterface.DisplayWelcomeScreen();
+            userInterface.DisplayAccountScreen();
+            userInterface.DisplayPoliceReportScreen();
+
+            while (true)
+            {
+                engine.SetMonthlyMinimalPopularityAndStrength();
+                engine.SetMonthlyRevolutionStrength();
+                engine.AdvanceMonth();
+                userInterface.DisplayMonthScreen();
+
+                // TODO: engine.Plot()
+                if (engine.IsGovernmentBankrupt())
+                {
+                    userInterface.DisplayBankuptcyScreen();
+                    // TODO: engine.Plot()
+                    ProcessPoliceReport();
+                }
+
+                // TODO: audience > this.userInterface.DisplayRequestScreen();
+                // TODO: plot
+
+                // TODO: if(engine.AssassinationAtempt() or engine.WarHasStarted())
+                // TODO: engine.End(); break;
+
+                // TODO: plot
+                ProcessPoliceReport();
+                userInterface.DisplayMainDecisionDialog();
+                ProcessPoliceReport();
+
+                if (engine.ShouldNewsHappen())
+                {
+                    if(engine.TryTriggerRandomUnusedNews())
+                    {
+                        userInterface.DisplayNewsScreen();
+                        // TODO: ApplyNewsEffects
+                        // TODO: plot
+                        ProcessPoliceReport();
+                    }
+                }
+
+                // TODO: if(engine.Revolution())
+                // TODO: engine.End(); break;
+            }
+        }
+
+        public void ProcessPoliceReport()
+        {
+            if (userInterface.DisplayPoliceReportRequestDialog())
+            {
+                userInterface.DisplayPoliceReportScreen();
+            }
+        }
+    }
+}
