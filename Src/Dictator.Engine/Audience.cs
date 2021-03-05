bed4bca6c61190fe341effaeca7f9dee08d9d2a7
Sergio Vicente <sfvicente@gmail.com>
2021-03-05 23:53:58 +0000

    Added the scaffold for the audience required classes

---
+++ Src/Dictator.Engine/Audience.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Core
+{
+    public class Audience : EventAction
+    {
+        public GroupType Requester { get; set; }
+
+        public Audience(GroupType requester, int cost, int monthlyCost, string groupPopularityChanges, string groupStrenghtChanges, string text)
+        {
+            this.Requester = requester;
+            this.Cost = cost;
+            this.MonthlyCost = monthlyCost;
+            this.GroupPopularityChanges = groupPopularityChanges;
+            this.GroupStrenghtChanges = groupStrenghtChanges;
+            this.Text = text;
+            this.HasBeenUsed = false;
+        }
+    }
+}
---
+++ Src/Dictator.Engine/AudienceStats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Core
+{
+    public class AudienceStats
+    {
+        private Audience[] audiences;
+
+        public AudienceStats()
+        {
+            Initialise();
+        }
+
+        public void Initialise()
+        {
+            audiences = new Audience[] {
+
+            };
+        }
+
+    }
+}
