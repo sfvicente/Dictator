3ed9d3b4e1d2d12a9dbb78627b60e78aef413292
Sergio Vicente <sfvicente@gmail.com>
2021-03-06 21:58:28 +0000

    Added first version of the end screen without any integrations

--- Src/Dictator.ConsoleInterface/EndScreen.cs
+++ Src/Dictator.ConsoleInterface/EndScreen.cs
@@ -1,4 +1,5 @@
-﻿using Dictator.Core;
+﻿using Dictator.Common.Extensions;
+using Dictator.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,12 +8,14 @@ namespace Dictator.ConsoleInterface
 {
     public class EndScreen : IEndScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
         private readonly IAccount account;
         private readonly IGovernmentStats governmentStats;
         private readonly IGroupStats groupStats;
 
-        public EndScreen(IAccount account, IGovernmentStats governmentStats, IGroupStats groupStats)
+        public EndScreen(IPressAnyKeyControl pressAnyKeyControl, IAccount account, IGovernmentStats governmentStats, IGroupStats groupStats)
         {
+            this.pressAnyKeyControl = pressAnyKeyControl;
             this.account = account;
             this.governmentStats = governmentStats;
             this.groupStats = groupStats;
@@ -20,7 +23,15 @@ namespace Dictator.ConsoleInterface
 
         public void Show()
         {
-            throw new NotImplementedException();
+            ConsoleEx.WriteAt(24, 3, "    Your RATING as PRESIDENT    ");
+            ConsoleEx.WriteAt(24, 5, " Total POPULARITY         - {totalPopularity}  ");
+            ConsoleEx.WriteAt(24, 7, " MONTHS in OFFICE ({month}x3)");
+
+            // TODO: count alive score
+
+            // TODO: save history if it is a new highscore
+
+            pressAnyKeyControl.Show();
         }
     }
 }
