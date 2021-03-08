df32970a64e5032cff37c4144b8bec944227ecb9
Sergio Vicente <sfvicente@gmail.com>
2021-03-08 23:37:27 +0000

    Added initial version of the visual representation of the escape attempt screen

--- Src/Dictator.ConsoleInterface/EscapeAttemptScreen.cs
+++ Src/Dictator.ConsoleInterface/EscapeAttemptScreen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +9,15 @@ namespace Dictator.ConsoleInterface
     {
         public void Show()
         {
+            ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Black);
+            ConsoleEx.WriteAt(24, 12, "        ESCAPE ATTEMPT ?        ");
+            
+            ConsoleKey keyPressed = Console.ReadKey(true).Key;
 
+            if(keyPressed == ConsoleKey.Y)
+            {
+                // TODO: process response
+            }
         }
     }
 }
