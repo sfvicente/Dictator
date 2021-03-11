e15be615dc04efd85085913c1b5a99a196451534
Sergio Vicente <sfvicente@gmail.com>
2021-03-11 00:43:32 +0000

    Added dialog result class to allow communication between game, engine and user interface

---
+++ Src/Dictator.ConsoleInterface/DialogResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.ConsoleInterface
+{
+    public enum DialogResult
+    {
+        Yes = 0,
+        No = 1
+    }
+}
