9ea3cece444793f0d9eed1269fd03681a278bfaa
Sergio Vicente <sfvicente@gmail.com>
2021-03-14 23:41:53 +0000

    Added first design representation for the audience decision dialog

--- Src/Dictator.ConsoleInterface/AudienceDecisionDialog.cs
+++ Src/Dictator.ConsoleInterface/AudienceDecisionDialog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,7 +16,28 @@ namespace Dictator.ConsoleInterface
 
         public DialogResult Show()
         {
-            throw new NotImplementedException();
+            ConsoleEx.Clear(ConsoleColor.DarkYellow);
+            ConsoleEx.WriteAt(11, 2, "  DECISION  ", ConsoleColor.Black, ConsoleColor.White);
+            ConsoleEx.WriteAt(5, 4, "The {GroupName} ask you to", ConsoleColor.Magenta, ConsoleColor.Gray);
+            ConsoleEx.WriteAt(5, 4, "{RequestText}", ConsoleColor.Yellow, ConsoleColor.Black);
+            ConsoleEx.WriteEmptyLineAt(8, ConsoleColor.Blue);
+
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            ConsoleEx.WriteAt(2, 10, "This decision would", ConsoleColor.Black);
+            ConsoleEx.WriteAt(2, 12, "TAKE from the TREASURY $120,000", ConsoleColor.Black);
+            ConsoleEx.WriteAt(2, 14, "and", ConsoleColor.Black);
+            ConsoleEx.WriteAt(2, 16, "RAISE MONTHLY COSTS bt $10,000", ConsoleColor.Black);
+
+            pressAnyKeyWithYesControl.Show();
+
+            ConsoleKey keyPressed = Console.ReadKey(true).Key;
+
+            if (keyPressed == ConsoleKey.Y)
+            {
+                return DialogResult.Yes;
+            }
+
+            return DialogResult.No;
         }
     }
 }
