935b03a3d0e57e61bd4da87dc6f4b5885a1b1817
Sergio Vicente <sfvicente@gmail.com>
2021-03-14 21:44:06 +0000

    Cleaned and streamlined the code in the treasury report screen

--- Src/Dictator.ConsoleInterface/TreasuryReportScreen.cs
+++ Src/Dictator.ConsoleInterface/TreasuryReportScreen.cs
@@ -1,15 +1,13 @@
 ﻿using Dictator.Common.Extensions;
 using Dictator.Core;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Dictator.ConsoleInterface
 {
     public class TreasuryReportScreen : ITreasuryReportScreen
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
-        public IAccount account;
+        private readonly IAccount account;
 
         public TreasuryReportScreen(IPressAnyKeyControl pressAnyKeyControl, IAccount account)
         {
@@ -20,24 +18,20 @@ namespace Dictator.ConsoleInterface
         public void Show()
         {
             ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Green);
-            
-            for(int row = 1; row < 22; row++)
-            {
-                ConsoleEx.WriteAt(1, row, "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-            }
+            ConsoleEx.Clear('$');
             ConsoleEx.WriteAt(2, 4, "                              ", ConsoleColor.Green, ConsoleColor.Black);
             ConsoleEx.WriteAt(2, 5, "                              ", ConsoleColor.Green, ConsoleColor.Black);
             ConsoleEx.WriteAt(2, 6, "                              ", ConsoleColor.Green, ConsoleColor.Black);
             ConsoleEx.WriteAt(8, 9, "TREASURY REPORT", ConsoleColor.White, ConsoleColor.Black);
 
-            string balanceWording = (this.account.TreasuryBalance > 0) ? "holds" : "OWES";
-            
-            ConsoleEx.WriteAt(2, 13, $" The TREASURY {balanceWording} ${this.account.TreasuryBalance},000 ", ConsoleColor.Blue, ConsoleColor.White);
-            ConsoleEx.WriteAt(3, 15, $" MONTHLY COSTS are ${this.account.MonthlyCosts},000 ", ConsoleColor.Blue, ConsoleColor.White);
+            string balanceWording = (account.TreasuryBalance > 0) ? "holds" : "OWES";
 
-            if (this.account.HasSwissBankAccount)
+            ConsoleEx.WriteAt(2, 13, $" The TREASURY {balanceWording} ${account.TreasuryBalance},000 ", ConsoleColor.Blue, ConsoleColor.White);
+            ConsoleEx.WriteAt(3, 15, $" MONTHLY COSTS are ${account.MonthlyCosts},000 ", ConsoleColor.Blue, ConsoleColor.White);
+
+            if (account.HasSwissBankAccount)
             {
-                ConsoleEx.WriteAt(3, 17, $" [SWISS Acct holds ${this.account.SwissBankAccountBalance},000] ");
+                ConsoleEx.WriteAt(3, 17, $" [SWISS Acct holds ${account.SwissBankAccountBalance},000] ");
             }
 
             pressAnyKeyControl.Show();
