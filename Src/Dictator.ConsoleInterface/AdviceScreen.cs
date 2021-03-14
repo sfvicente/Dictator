026a893c34a9bf5685ba186474894557f8fbbf29
Sergio Vicente <sfvicente@gmail.com>
2021-03-14 22:03:35 +0000

    Added initial graphical representation for the advice screen

--- Src/Dictator.ConsoleInterface/AdviceScreen.cs
+++ Src/Dictator.ConsoleInterface/AdviceScreen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,29 @@ namespace Dictator.ConsoleInterface
 {
     public class AdviceScreen : IAdviceScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public AdviceScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
+            ConsoleEx.Clear(ConsoleColor.DarkYellow);
+            ConsoleEx.WriteAt(1, 2, "{Decision Text}", ConsoleColor.Black, ConsoleColor.DarkYellow);
+            ConsoleEx.WriteAt(1, 4, "Your POPULARITY with", ConsoleColor.Yellow, ConsoleColor.Black);
+            Console.Write(" ....");
 
+            // TODO: display decision impact on each group popularity
+
+            ConsoleEx.WriteAt(1, 11, "The STRENGTH of", ConsoleColor.Yellow, ConsoleColor.Black);
+            Console.Write(" ....");
+
+            // TODO: display decision impact on each group strength
+
+            pressAnyKeyControl.Show();
+            Console.ReadKey(true);
         }
     }
 }
