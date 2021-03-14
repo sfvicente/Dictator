790868f5f26fe6fcc71ac41f8e8093a78be6dc0b
Sergio Vicente <sfvicente@gmail.com>
2021-03-14 21:38:39 +0000

    Improved the title screen design to better resemble the original game

--- Src/Dictator.ConsoleInterface/TitleScreen.cs
+++ Src/Dictator.ConsoleInterface/TitleScreen.cs
@@ -27,13 +27,41 @@ namespace Dictator.ConsoleInterface
 
         public void DrawContent()
         {
-            ConsoleEx.WriteAt(1, 3, "Press any key to become DICTATOR", ConsoleColor.White, ConsoleColor.Black);
-            ConsoleEx.WriteAt(13, 5, " of the ", ConsoleColor.White, ConsoleColor.Black);
-            ConsoleEx.WriteAt(13, 8, "RITIMBAN", ConsoleColor.Green, ConsoleColor.Black);
-            ConsoleEx.WriteAt(14, 10, "******", ConsoleColor.Blue, ConsoleColor.Yellow);
-            ConsoleEx.WriteAt(16, 11, "**", ConsoleColor.Blue, ConsoleColor.Yellow);
-            ConsoleEx.WriteAt(14, 12, "******", ConsoleColor.Blue, ConsoleColor.Yellow);
-            ConsoleEx.WriteAt(13, 14, "REPUBLIC", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 4, "Press any key to become DICTATOR", ConsoleColor.Gray, ConsoleColor.Black);
+            ConsoleEx.WriteAt(13, 6, " of the ", ConsoleColor.Gray, ConsoleColor.Black);
+            PrintFlagLine(9);
+            PrintFlagLine(10);
+            ConsoleEx.WriteAt(8, 11, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(12, 11, " ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(13, 11, "RITIMBAN", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(21, 11, " ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(22, 11, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            PrintFlagLine(12);
+            ConsoleEx.WriteAt(8, 13, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(12, 13, "  ******  ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(22, 13, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(8, 14, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(12, 14, "    **    ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(22, 14, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(8, 15, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(12, 15, "  ******  ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(22, 15, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            PrintFlagLine(16);
+            ConsoleEx.WriteAt(8, 17, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(12, 17, " ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(13, 17, "REPUBLIC", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(21, 17, " ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(22, 17, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            PrintFlagLine(18);
+            PrintFlagLine(19);
+            PrintFlagLine(20);
+        }
+
+        private void PrintFlagLine(int row)
+        {
+            ConsoleEx.WriteAt(8, row, "    ", ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleEx.WriteAt(12, row, "          ", ConsoleColor.Blue, ConsoleColor.Yellow);
+            ConsoleEx.WriteAt(22, row, "    ", ConsoleColor.Green, ConsoleColor.Black);
         }
     }
 }
