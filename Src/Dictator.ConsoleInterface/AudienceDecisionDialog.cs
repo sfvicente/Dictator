beba24128ec3b503375cd18a1c2b73196459ccea
Sergio Vicente <sfvicente@gmail.com>
2021-03-16 23:05:31 +0000

    Added initial graphical implementation of the audience decision dialog

--- Src/Dictator.ConsoleInterface/AudienceDecisionDialog.cs
+++ Src/Dictator.ConsoleInterface/AudienceDecisionDialog.cs
@@ -1,4 +1,5 @@
 ﻿using Dictator.Common.Extensions;
+using Dictator.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,26 +8,46 @@ namespace Dictator.ConsoleInterface
 {
     public class AudienceDecisionDialog : IAudienceDecisionDialog
     {
+        private readonly IAudienceStats audienceStats;
         private readonly IPressAnyKeyWithYesControl pressAnyKeyWithYesControl;
 
-        public AudienceDecisionDialog(IPressAnyKeyWithYesControl pressAnyKeyWithYesControl)
+        public AudienceDecisionDialog(IAudienceStats audienceStats, IPressAnyKeyWithYesControl pressAnyKeyWithYesControl)
         {
+            this.audienceStats = audienceStats;
             this.pressAnyKeyWithYesControl = pressAnyKeyWithYesControl;
         }
 
         public DialogResult Show()
         {
+            Audience audience = audienceStats.CurrentAudienceRequest;
+
             ConsoleEx.Clear(ConsoleColor.DarkYellow);
             ConsoleEx.WriteAt(11, 2, "  DECISION  ", ConsoleColor.Black, ConsoleColor.White);
-            ConsoleEx.WriteAt(5, 4, "The {GroupName} ask you to", ConsoleColor.Magenta, ConsoleColor.Gray);
-            ConsoleEx.WriteAt(5, 4, "{RequestText}", ConsoleColor.Yellow, ConsoleColor.Black);
+            ConsoleEx.WriteAt(5, 4, $"The {audience.Requester} ask you to", ConsoleColor.Magenta, ConsoleColor.Gray);
+            ConsoleEx.WriteAt(5, 4, $"{audience.Text}", ConsoleColor.Yellow, ConsoleColor.Black);
             ConsoleEx.WriteEmptyLineAt(8, ConsoleColor.Blue);
 
             Console.BackgroundColor = ConsoleColor.DarkYellow;
-            ConsoleEx.WriteAt(2, 10, "This decision would", ConsoleColor.Black);
-            ConsoleEx.WriteAt(2, 12, "TAKE from the TREASURY $120,000", ConsoleColor.Black);
-            ConsoleEx.WriteAt(2, 14, "and", ConsoleColor.Black);
-            ConsoleEx.WriteAt(2, 16, "RAISE MONTHLY COSTS bt $10,000", ConsoleColor.Black);
+
+            if (audience.Cost != 0 || audience.MonthlyCost != 0)
+            {
+                ConsoleEx.WriteAt(2, 10, "This decision would", ConsoleColor.Black);
+            }
+
+            if (audience.Cost != 0)
+            {
+                ConsoleEx.WriteAt(2, 12, "TAKE from the TREASURY $120,000", ConsoleColor.Black);
+            }
+
+            if (audience.Cost != 0 && audience.MonthlyCost != 0)
+            {
+                ConsoleEx.WriteAt(2, 14, "and", ConsoleColor.Black);
+            }
+
+            if (audience.MonthlyCost != 0)
+            {
+                ConsoleEx.WriteAt(2, 16, "RAISE MONTHLY COSTS bt $10,000", ConsoleColor.Black);
+            }
 
             pressAnyKeyWithYesControl.Show();
 
