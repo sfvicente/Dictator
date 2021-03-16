39e24bbdb8cfc7d8c8b483b0df0ed208639ec1c4
Sergio Vicente <sfvicente@gmail.com>
2021-03-16 23:23:30 +0000

    Added initial graphical implementation of the war screen

--- Src/Dictator.ConsoleInterface/WarScreen.cs
+++ Src/Dictator.ConsoleInterface/WarScreen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +9,14 @@ namespace Dictator.ConsoleInterface
     {
         public void Show()
         {
+            ConsoleEx.Clear(ConsoleColor.Red);
+            ConsoleEx.WriteAt(7, 8, " LEFTOTO  INVADES ", ConsoleColor.Black, ConsoleColor.White);
+            ConsoleEx.WriteAt(1, 12, "     Ritimban Strength is XX    ", ConsoleColor.Red, ConsoleColor.Gray);
+            ConsoleEx.WriteAt(1, 14, "     Leftotan Strength is YY    ", ConsoleColor.Red, ConsoleColor.Gray);
+            ConsoleEx.WriteAt(6, 18, "A SHORT DECISIVE WAR", ConsoleColor.White, ConsoleColor.Black);
 
+            // TODO: replace with x seconds delay
+            Console.ReadKey(true);
         }
     }
 }
