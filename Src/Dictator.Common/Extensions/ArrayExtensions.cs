ccfce4c4479df9a9c8a4d76a1869dd51584a5a85
Sergio Vicente <sfvicente@gmail.com>
2021-03-17 21:53:59 +0000

    Added the IsEmpty array extension to make code easier to read when checking arrays for no elements

---
+++ Src/Dictator.Common/Extensions/ArrayExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Common.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static bool IsEmpty<T>(this T[] array)
+        {
+            return array.Length == 0;
+        }
+    }
+}
