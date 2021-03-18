43ed768d0ea32757a368ef09a83158ec99573d2d
Sergio Vicente <sfvicente@gmail.com>
2021-03-18 02:39:06 +0000

    Added the Abs extension method for the Int32 class

---
+++ Src/Dictator.Common/Extensions/IntegerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Common.Extensions
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        ///     Returns the absolute value of an integer number.
+        /// </summary>
+        /// <param name="value">A 32-bit signed integer.</param>
+        /// <returns>Returns the absolute value of a 32-bit signed integer.</returns>
+        public static int Abs(this int value)
+        {
+            return Math.Abs(value);
+        }
+    }
+}
