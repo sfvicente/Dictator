42e5f6764703814f4d806118a87788a4046763c6
Sergio Vicente <sfvicente@gmail.com>
2021-03-19 23:05:28 +0000

    Set all IGovernmentStats as public to better make intentions clear

--- Src/Dictator.Engine/IGovernmentStats.cs
+++ Src/Dictator.Engine/IGovernmentStats.cs
@@ -6,17 +6,17 @@ namespace Dictator.Core
 {
     public interface IGovernmentStats
     {
-        bool IsPlayerAlive { get; }
-        bool HasHelicopter { get; }
-        int PlayerStrength { get; }
-        int Month { get; }
-        int PlotBonus { get; set; }
-        int MonthlyRevolutionStrength { get; set; }
-        int MonthlyMinimalPopularityAndStrength { get; set; }
+        public bool IsPlayerAlive { get; }
+        public bool HasHelicopter { get; }
+        public int PlayerStrength { get; }
+        public int Month { get; }
+        public int PlotBonus { get; set; }
+        public int MonthlyRevolutionStrength { get; set; }
+        public int MonthlyMinimalPopularityAndStrength { get; set; }
 
-        void Initialise();
-        void AdvanceMonth();
-        void DecreasePlayerStrength();
-        void KillPlayer();
+        public void Initialise();
+        public void AdvanceMonth();
+        public void DecreasePlayerStrength();
+        public void KillPlayer();
     }
 }
