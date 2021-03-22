ee60ea9c4a659589e8bf86c48e1a3bd11c37299e
Sergio Vicente <sfvicente@gmail.com>
2021-03-22 01:27:23 +0000

    Added the creation of the audience data for the peasants group

--- Src/Dictator.Engine/AudienceStats.cs
+++ Src/Dictator.Engine/AudienceStats.cs
@@ -39,8 +39,16 @@ namespace Dictator.Core
                 new Audience(GroupType.Army, 0, 0, "PMMMLMIO", "MMMMMM", "EXPEL RUSSIAN MILITARY ADVISORS "),
                 new Audience(GroupType.Army, 0, 0/*D*/, "QMLMMMMM", "OLLLMM", " INCREASE the PAY of the TROOPS "),
                 new Audience(GroupType.Army, 0/*A*/, 0, "QLLMLLMM", "PLLKLM", "  BUY MORE ARMS and AMMUNITION  "),
+                new Audience(GroupType.Peasants, 0, 0/*P*/, "LONMMMMM", "LMMLMM", "   STOP ARMY SIGN-UP COERCION   "),
+                new Audience(GroupType.Peasants, 0, 0, "MQIMNMMM", "MOLMMM", "INCREASE the BASIC MINIMUM WAGE "),
+                new Audience(GroupType.Peasants, 0, 0, "NQOMMIMM", "NNNNMJ", " CUT the POWERS of the S.POLICE "),
+                new Audience(GroupType.Peasants, 0, 0, "MPKMKMMM", "MOKMMM", "STOP LEFTOTAN IMMIGRANT WORKERS "),
+                new Audience(GroupType.Peasants, 0/*C*/, 0/*E*/, "LQKMOLNM", "MNLLMM", "INTRODUCE FREE EDUCATION for ALL"),
+                new Audience(GroupType.Peasants, 0, 0, "MQJMNLNM", "MPJMML", "LEGALISE the FORMATION of UNIONS"),
+                new Audience(GroupType.Peasants, 0, 0, "LQKMNLMM", "MOLLMM", "  FREE their IMPRISONED LEADER  "),
+                new Audience(GroupType.Peasants, 0, 0/*S*/, "MPLMMMMM", "MMMLMM", "     START a PUBLIC LOTTERY     "),
 
-                // TODO: add peasants and land owners petition data
+                // TODO: add land owners petition data
             };
         }
 
