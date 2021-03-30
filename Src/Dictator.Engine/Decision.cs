b7c5d710f384f95664c395f22eb829306ebfefd0
Sergio Vicente <sfvicente@gmail.com>
2021-03-30 23:03:40 +0100

    Refactored the Decision classes to allow for sub-types which is required for special decisions such as increase bodyguard or transfer to swiss account

--- Src/Dictator.Engine/Decision.cs
+++ Src/Dictator.Engine/Decision.cs
@@ -7,10 +7,19 @@ namespace Dictator.Core
     public class Decision: EventAction
     {
         public DecisionType Type { get; set; }
+        public DecisionSubType DecisionSubType { get; set; }
 
-        public Decision(DecisionType type, int cost, int monthlyCost, string groupPopularityChanges, string groupStrenghtChanges, string text)
+        public Decision(
+            DecisionType type,
+            DecisionSubType decisionSubType,
+            int cost,
+            int monthlyCost,
+            string groupPopularityChanges,
+            string groupStrenghtChanges,
+            string text)
         {
             Type = type;
+            DecisionSubType = decisionSubType;
             Cost = cost;
             MonthlyCost = monthlyCost;
             GroupPopularityChanges = groupPopularityChanges;
