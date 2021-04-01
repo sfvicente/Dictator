53d74967d80d7e1fc0b149af06582232cfd1b75c
Sergio Vicente <sfvicente@gmail.com>
2021-04-01 23:12:19 +0100

    Added graphical implementation of the WarThreatScreen

--- Src/Dictator.ConsoleInterface/War/WarThreatScreen.cs
+++ Src/Dictator.ConsoleInterface/War/WarThreatScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using Dictator.ConsoleInterface.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,20 @@ namespace Dictator.ConsoleInterface.War
 {
     public class WarThreatScreen : IWarThreatScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public WarThreatScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
-
+            ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 6, "   THREAT of WAR with LEFTOTO   ");
+            ConsoleEx.WriteAt(1, 11, "   Your POPULARITY in RITIMBA   ");
+            ConsoleEx.WriteAt(1, 12, "            will RISE           ");
+            pressAnyKeyControl.Show();
         }
     }
 }
