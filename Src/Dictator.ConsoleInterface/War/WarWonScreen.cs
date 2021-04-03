090ddec0a42cf6d4c8b58f237faf40a7e7f8018b
Sergio Vicente <sfvicente@gmail.com>
2021-04-03 02:22:14 +0100

    Added implementation of the graphical representation of the WarWonScreen

--- Src/Dictator.ConsoleInterface/War/WarWonScreen.cs
+++ Src/Dictator.ConsoleInterface/War/WarWonScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using Dictator.ConsoleInterface.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,17 @@ namespace Dictator.ConsoleInterface.War
 {
     public class WarWonScreen : IWarWonScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public WarWonScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
-
+            ConsoleEx.WriteAt(1, 11, "        LEFTOTANS ROUTED        ");
+            pressAnyKeyControl.Show();
         }
     }
 }
