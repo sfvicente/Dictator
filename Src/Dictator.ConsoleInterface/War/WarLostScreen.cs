a07faea708b7eed23d81bf5766cf964c87713dd1
Sergio Vicente <sfvicente@gmail.com>
2021-04-03 02:20:51 +0100

    Added implementation of the graphical representation of the WarLostScreen

--- Src/Dictator.ConsoleInterface/War/WarLostScreen.cs
+++ Src/Dictator.ConsoleInterface/War/WarLostScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using Dictator.ConsoleInterface.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,17 @@ namespace Dictator.ConsoleInterface.War
 {
     public class WarLostScreen : IWarLostScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public WarLostScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
-            throw new NotImplementedException();
+            ConsoleEx.WriteAt(1, 7, "        LEFTOTAN VICTORY        ");
+            pressAnyKeyControl.Show();
         }
     }
 }
