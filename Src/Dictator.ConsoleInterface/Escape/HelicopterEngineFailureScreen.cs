0b19d2cdaa795c2dfe569f3f09d3622826035f83
Sergio Vicente <sfvicente@gmail.com>
2021-04-03 02:18:53 +0100

    Added implementation of the graphical representation of the HelicopterEngineFailure screen

--- Src/Dictator.ConsoleInterface/Escape/HelicopterEngineFailureScreen.cs
+++ Src/Dictator.ConsoleInterface/Escape/HelicopterEngineFailureScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using Dictator.ConsoleInterface.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,21 @@ namespace Dictator.ConsoleInterface.Escape
 {
     public class HelicopterEngineFailureScreen : IHelicopterEngineFailureScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public HelicopterEngineFailureScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
-            throw new NotImplementedException();
+            ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 9, "    Helicopter ENGINE FAILURE   ");
+            ConsoleEx.WriteAt(1, 11, "     YOU are judged to be an    ");
+            ConsoleEx.WriteAt(1, 13, "   ENEMY of the PEOPLE and ...  ");
+            ConsoleEx.WriteAt(1, 15, "       Summarily EXECUTED       ");
+            pressAnyKeyControl.Show();
         }
     }
 }
