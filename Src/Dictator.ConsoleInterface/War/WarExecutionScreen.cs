7a712c27d1f185ba7cad0b67929b883b374fd2e2
Sergio Vicente <sfvicente@gmail.com>
2021-04-07 03:23:05 +0100

    Added implementation of the graphic representation of the war execution screen

--- Src/Dictator.ConsoleInterface/War/WarExecutionScreen.cs
+++ Src/Dictator.ConsoleInterface/War/WarExecutionScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using Dictator.ConsoleInterface.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,20 @@ namespace Dictator.ConsoleInterface.War
 {
     public class WarExecutionScreen : IWarExecutionScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public WarExecutionScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
-            throw new NotImplementedException();
+            ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 11, "     YOU are judged to be an    ");
+            ConsoleEx.WriteAt(1, 13, "   ENEMY of the PEOPLE and ...  ");
+            ConsoleEx.WriteAt(1, 15, "       Summarily EXECUTED       ");
+            pressAnyKeyControl.Show();
         }
     }
 }
