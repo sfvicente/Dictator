b5848218584b3bfef070b4d31e1fb7712f894ea9
Sergio Vicente <sfvicente@gmail.com>
2021-04-12 01:39:17 +0100

    Added the graphical implementation of the guerrilas missed screen

--- Src/Dictator.ConsoleInterface/Escape/GuerillasMissedScreen.cs
+++ Src/Dictator.ConsoleInterface/Escape/GuerillasMissedScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dictator.Common.Extensions;
+using Dictator.ConsoleInterface.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,18 @@ namespace Dictator.ConsoleInterface.Escape
 {
     public class GuerillasMissedScreen : IGuerillasMissedScreen
     {
+        private readonly IPressAnyKeyControl pressAnyKeyControl;
+
+        public GuerillasMissedScreen(IPressAnyKeyControl pressAnyKeyControl)
+        {
+            this.pressAnyKeyControl = pressAnyKeyControl;
+        }
+
         public void Show()
         {
-            throw new NotImplementedException();
+            ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 11, " The GUERILLAS didn't catch you ");
+            pressAnyKeyControl.Show();
         }
     }
 }
