3d68ddea6a98a5f0bac797809f76d15c46ccac5a
Sergio Vicente <sfvicente@gmail.com>
2021-04-14 02:45:51 +0100

    Replaced the hardcoded monetary values in the audience decision screen

--- Src/Dictator.ConsoleInterface/Audience/AudienceDecisionDialog.cs
+++ Src/Dictator.ConsoleInterface/Audience/AudienceDecisionDialog.cs
@@ -35,7 +35,7 @@ namespace Dictator.ConsoleInterface.Audience
 
             if (audience.Cost != 0)
             {
-                ConsoleEx.WriteAt(2, 12, "TAKE from the TREASURY $120,000", ConsoleColor.Black);
+                ConsoleEx.WriteAt(2, 12, $"TAKE from the TREASURY ${audience.Cost},000", ConsoleColor.Black);
             }
 
             if (audience.Cost != 0 && audience.MonthlyCost != 0)
@@ -45,7 +45,7 @@ namespace Dictator.ConsoleInterface.Audience
 
             if (audience.MonthlyCost != 0)
             {
-                ConsoleEx.WriteAt(2, 16, "RAISE MONTHLY COSTS by $10,000", ConsoleColor.Black);
+                ConsoleEx.WriteAt(2, 16, $"RAISE MONTHLY COSTS by ${audience.MonthlyCost},000", ConsoleColor.Black);
             }
 
             return pressAnyKeyWithYesControl.Show();
