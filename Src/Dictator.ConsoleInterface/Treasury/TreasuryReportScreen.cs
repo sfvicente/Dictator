0f54c82b1ecd8b87722400a9829710d45a35a7d1
Sergio Vicente <sfvicente@gmail.com>
2021-04-17 01:04:45 +0100

    Refactored the treasury report screen to use the account control

--- Src/Dictator.ConsoleInterface/Treasury/TreasuryReportScreen.cs
+++ Src/Dictator.ConsoleInterface/Treasury/TreasuryReportScreen.cs
@@ -8,10 +8,12 @@ namespace Dictator.ConsoleInterface.Treasury
 {
     public class TreasuryReportScreen : ITreasuryReportScreen
     {
+        private readonly IAccountControl accountControl;
         private readonly IPressAnyKeyControl pressAnyKeyControl;
 
-        public TreasuryReportScreen(IPressAnyKeyControl pressAnyKeyControl)
+        public TreasuryReportScreen(IAccountControl accountControl, IPressAnyKeyControl pressAnyKeyControl)
         {
+            this.accountControl = accountControl;
             this.pressAnyKeyControl = pressAnyKeyControl;
         }
 
@@ -23,17 +25,7 @@ namespace Dictator.ConsoleInterface.Treasury
             ConsoleEx.WriteAt(2, 5, "                              ", ConsoleColor.Green, ConsoleColor.Black);
             ConsoleEx.WriteAt(2, 6, "                              ", ConsoleColor.Green, ConsoleColor.Black);
             ConsoleEx.WriteAt(8, 9, "TREASURY REPORT", ConsoleColor.White, ConsoleColor.Black);
-
-            string balanceWording = (account.TreasuryBalance > 0) ? "holds" : "OWES";
-
-            ConsoleEx.WriteAt(2, 13, $" The TREASURY {balanceWording} ${account.TreasuryBalance},000 ", ConsoleColor.Blue, ConsoleColor.White);
-            ConsoleEx.WriteAt(3, 15, $" MONTHLY COSTS are ${account.MonthlyCosts},000 ", ConsoleColor.Blue, ConsoleColor.White);
-
-            if (account.HasSwissBankAccount)
-            {
-                ConsoleEx.WriteAt(3, 18, $"[SWISS Acct holds ${account.SwissBankAccountBalance},000]", ConsoleColor.Blue, ConsoleColor.White);
-            }
-
+            accountControl.Show(account);
             pressAnyKeyControl.Show();
         }
     }
