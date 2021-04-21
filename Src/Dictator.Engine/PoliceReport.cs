6d70bec8058cd7ed236ef4097f8fdabbeb8dd65a
Sergio Vicente <sfvicente@gmail.com>
2021-04-21 22:05:35 +0100

    Added PoliceReport core type for subsequent refactoring

---
+++ Src/Dictator.Engine/PoliceReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Dictator.Core
+{
+    public class PoliceReport
+    {
+        public int Month { get; set; }
+        public ReadOnlyCollection<Group> Groups { get; set; }
+        public int PlayerStrength { get; set; }
+        public int MonthlyRevolutionStrength { get; set; }
+    }
+}
