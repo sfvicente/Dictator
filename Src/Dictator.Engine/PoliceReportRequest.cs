2f8e1528d615732e230bbd91c20a58f9b823d613
Sergio Vicente <sfvicente@gmail.com>
2021-04-21 22:09:05 +0100

    Added the PoliceReportRequest to the core types for subsequent refactoring

---
+++ Src/Dictator.Engine/PoliceReportRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Core
+{
+    public class PoliceReportRequest
+    {
+        public bool HasEnoughBalance { get; set; }
+        public bool HasEnoughPopularityWithPolice { get; set; }
+        public bool HasPoliceEnoughStrength { get; set; }
+        public int PoliceStrength { get; set; }
+    }
+}
