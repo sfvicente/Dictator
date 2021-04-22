265440efc28857b27edfb511f9d3d7289917592f
Sergio Vicente <sfvicente@gmail.com>
2021-04-22 21:53:52 +0100

    Added documentation for the PoliceReport class

--- Src/Dictator.Engine/PoliceReport.cs
+++ Src/Dictator.Engine/PoliceReport.cs
@@ -5,11 +5,29 @@ using System.Text;
 
 namespace Dictator.Core
 {
+    /// <summary>
+    ///     Represents a detailed account or statement from police on the state of the groups and government.
+    /// </summary>
     public class PoliceReport
     {
+        /// <summary>
+        ///     Gets or sets the current month of government. 
+        /// </summary>
         public int Month { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the detailed group information. 
+        /// </summary>
         public ReadOnlyCollection<Group> Groups { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the current player's strength.
+        /// </summary>
         public int PlayerStrength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the monthly generated revolution strength. 
+        /// </summary>
         public int MonthlyRevolutionStrength { get; set; }
     }
 }
