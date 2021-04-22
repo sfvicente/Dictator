ebdd6fce3d97f8f8b240c233543fbf54b2676b91
Sergio Vicente <sfvicente@gmail.com>
2021-04-22 21:49:02 +0100

    Added documention for the LoanApplicationResult class

--- Src/Dictator.Engine/LoanApplicationResult.cs
+++ Src/Dictator.Engine/LoanApplicationResult.cs
@@ -4,10 +4,24 @@ using System.Text;
 
 namespace Dictator.Core
 {
+    /// <summary>
+    ///     Represents the outcome of a loan application.
+    /// </summary>
     public class LoanApplicationResult
     {
+        /// <summary>
+        ///     Gets or sets if the loan application has been accepted by the country. 
+        /// </summary>
         public bool IsAccepted { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the amount of the loan provided by the country when the application has been accepted. 
+        /// </summary>
         public int Amount { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the country to which the loan application is made to. 
+        /// </summary>
         public Country Country { get; set; }
     }
 }
