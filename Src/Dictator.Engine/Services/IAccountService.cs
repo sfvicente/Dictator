b2a5a7a23911d56e59268b3cc36f67dbd3acf3b2
Sergio Vicente <sfvicente@gmail.com>
2021-04-23 11:15:51 +0100

    Documented all the method signatures of the IAccountService

--- Src/Dictator.Engine/Services/IAccountService.cs
+++ Src/Dictator.Engine/Services/IAccountService.cs
@@ -6,16 +6,63 @@ namespace Dictator.Core.Services
 {
     public interface IAccountService
     {
+        /// <summary>
+        ///     Initialises the account data.
+        /// </summary>
         public void Initialise();
+
+        /// <summary>
+        ///     Retrieves the current account aggregated properties.
+        /// </summary>
+        /// <returns></returns>
         public Account GetAccount();
+
+        /// <summary>
+        ///     Retrieves the current treasury balance.
+        /// </summary>
+        /// <returns></returns>
         public int GetTreasuryBalance();
+
+        /// <summary>
+        ///     Retrives the current monthly costs for the treasury.
+        /// </summary>
+        /// <returns></returns>
         public int GetMonthlyCosts();
+
+        /// <summary>
+        ///     Modifies both the current treasury balance and monthly costs.
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <param name="monthlyCost"></param>
         public void ApplyTreasuryChanges(int cost, int monthlyCost);
+
+        /// <summary>
+        ///     Modifies the treasure current balance.
+        /// </summary>
+        /// <param name="amount"></param>
         public void ChangeTreasuryBalance(int amount);
 
+        /// <summary>
+        ///     Determines if a Swiss account has been opened.
+        /// </summary>
+        /// <returns></returns>
         public bool HasSwissBankAccount();
+
+        /// <summary>
+        ///     Opens a Swiss bank account to allow for transfers to me made from treasury.
+        /// </summary>
         public void OpenSwissBankAccount();
+
+        /// <summary>
+        ///     Gets the current balance in the Swiss bank account.
+        /// </summary>
+        /// <returns></returns>
         public int GetSwissBankAccountBalance();
+
+        /// <summary>
+        ///     Deposits the specified amount into the Swiss bank account.
+        /// </summary>
+        /// <param name="amount"></param>
         public void DepositToSwissBankAccount(int amount);
     }
 }
