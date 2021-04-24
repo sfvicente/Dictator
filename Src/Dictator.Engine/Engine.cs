47dfd84fcc6c09bc3b81cedb891003a0fabb3f8a
Sergio Vicente <sfvicente@gmail.com>
2021-04-24 16:12:21 +0100

    Added comment to the ShouldWarHappen() method

--- Src/Dictator.Engine/Engine.cs
+++ Src/Dictator.Engine/Engine.cs
@@ -375,6 +375,10 @@ namespace Dictator.Core
             return true;
         }
 
+        /// <summary>
+        ///     Determines if a war between the Ritimba republic and Leftoto should happen. There is a 1/3 probability that war should happen.
+        /// </summary>
+        /// <returns><c>true</c> if war should happen; otherwise, <c>false</c>.</returns>
         public bool ShouldWarHappen()
         {
             Random random = new Random();
