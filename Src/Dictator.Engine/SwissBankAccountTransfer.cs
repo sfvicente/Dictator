1a172e13c2c00cfbac8e3fa3e67809cbf3ef0cd8
Sergio Vicente <sfvicente@gmail.com>
2021-04-24 16:49:27 +0100

    Added comments for the SwissBankAccountTransfer class and its methods

--- Src/Dictator.Engine/SwissBankAccountTransfer.cs
+++ Src/Dictator.Engine/SwissBankAccountTransfer.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace Dictator.Core
 {
+    /// <summary>
+    ///     Represents a bank transfer from the treasury to the Swiss bank account.
+    /// </summary>
     public class SwissBankAccountTransfer
     {
+        /// <summary>
+        ///     Gets or sets the amount that has been stolen from the treasury and transfered to the Swiss bank account.
+        /// </summary>
         public int AmountStolen { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the amount that was in the treasury prior to the transfer to the Swiss bank account.
+        /// </summary>
         public int TreasuryPreviousBalance { get; set; }
     }
 }
