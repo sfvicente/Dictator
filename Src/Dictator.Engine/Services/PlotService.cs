bb5cd37d6d46fbddb0680282f67d1e296c5b6fcd
Sergio Vicente <sfvicente@gmail.com>
2021-04-27 14:40:46 +0100

    Added the internal logic for the plot mechanic used to create the revolution and assassination scenarios

--- Src/Dictator.Engine/Services/PlotService.cs
+++ Src/Dictator.Engine/Services/PlotService.cs
@@ -18,7 +18,7 @@ namespace Dictator.Core.Services
         public void Plot()
         {
             // Do not trigger assassinations or revolutions in the first 2 months of government
-            if(governmentStats.Month < 2)
+            if (governmentStats.Month < 2)
             {
                 return;
             }
@@ -27,7 +27,46 @@ namespace Dictator.Core.Services
 
             // TODO: plot bonus check
 
-            // TODO: perform plot logic
+            Group[] groups = groupStats.GetGroups();
+            int monthlyMinimalPopularityAndStrength = governmentStats.MonthlyMinimalPopularityAndStrength;
+            int monthlyRevolutionStrength = governmentStats.MonthlyRevolutionStrength;
+
+            // Perform internal plot logic
+            ExecutePlot(groups, monthlyMinimalPopularityAndStrength, monthlyRevolutionStrength);
+        }
+
+        private void ExecutePlot(Group[] groups, int monthlyMinimalPopularityAndStrength, int monthlyRevolutionStrength)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Only groups with popularity less or equal to the monthly minimal popularity plot against the player
+                if (groups[i].Popularity <= monthlyMinimalPopularityAndStrength)
+                {
+                    // Cycle through the groups to find allies for the revolution
+                    for (int k = 0; k < 6; k++)
+                    {
+                        // Skip this pairing if we are dealing with the same group or a group that has popularity above the minimal monthly popularity required
+                        if (i == k || groups[k].Popularity > monthlyMinimalPopularityAndStrength)
+                            continue;
+
+                        // Verify if the strength of both groups combined is equal or greater than the monthly defined minimal revolution strength
+                        if (groups[i].Strength + groups[k].Strength >= monthlyRevolutionStrength)
+                        {
+                            // If so, set the ally for the original group and the appropriate status
+                            groups[i].Status = GroupStatus.Revolution;
+                            groups[i].Ally = groups[k];
+                            break;
+                        }
+                    }
+
+                    // If no allies could be found for the group, set the group status to assassination
+                    if (groups[i].Status == GroupStatus.Default)
+                    {
+                        groups[i].Status = GroupStatus.Assassination;
+                    }
+                }
+            }
+
         }
     }
 }
