f7b3d69c6e15cc248df25c988c49b2ba4998f568
Sergio Vicente <sfvicente@gmail.com>
2021-04-28 22:41:31 +0100

    Added the Revolution core type to be used in the revolution mechanic

---
+++ Src/Dictator.Engine/Revolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictator.Core
+{
+    public class Revolution
+    {
+        /// <summary>
+        ///     Gets or sets the current player's strength.
+        /// </summary>
+        public int PlayerStrength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the current revolution strength. 
+        /// </summary>
+        public int RevolutionStrength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the current group that has started the revolution.
+        /// </summary>
+        public Group RevolutionaryGroup { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the ally of the current group that has started the revolution.
+        /// </summary>
+        public Group RevolutionGroupAlly { get; set; }
+    }
+}
