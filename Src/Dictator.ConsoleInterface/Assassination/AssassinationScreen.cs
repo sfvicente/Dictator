0bc656979b2b3ec6199e506b089581fbebb21d5f
Sergio Vicente <sfvicente@gmail.com>
2021-04-29 23:11:34 +0100

    Fixed the display bugs of the assassination attempt that was not clearing the screen nor centering the group name

--- Src/Dictator.ConsoleInterface/Assassination/AssassinationScreen.cs
+++ Src/Dictator.ConsoleInterface/Assassination/AssassinationScreen.cs
@@ -22,8 +22,9 @@ namespace Dictator.ConsoleInterface.Assassination
         {
             GroupType assassinGroupType = groupStats.AssassinGroupType;
             string groupName = groupStats.GetGroupByType(assassinGroupType).Name;
-            int startPosition = (32 - groupName.Length) - 10 / 2;
+            int startPosition = (32 - groupName.Length) / 2;
 
+            ConsoleEx.Clear();
             ConsoleEx.WriteAt(1, 11, "      ASSASSINATION ATTEMPT     ");
             ConsoleEx.WriteAt(startPosition, 12, $"by one of {groupName}");
             pressAnyKeyControl.Show();
