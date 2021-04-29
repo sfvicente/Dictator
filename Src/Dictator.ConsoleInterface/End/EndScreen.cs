f7aa47fca4ab7d2d5d9a39a7e5e682db66b531a7
Sergio Vicente <sfvicente@gmail.com>
2021-04-29 23:19:31 +0100

    Fixed incorrect color and content in several areas of the end screen

--- Src/Dictator.ConsoleInterface/End/EndScreen.cs
+++ Src/Dictator.ConsoleInterface/End/EndScreen.cs
@@ -25,15 +25,17 @@ namespace Dictator.ConsoleInterface.End
 
         public void Show(Score score)
         {
+            // TODO: scores values need to be aligned to the right
+
             ConsoleEx.Clear(ConsoleColor.DarkYellow);
-            ConsoleEx.WriteAt(5, 3, "Your RATING as PRESIDENT", ConsoleColor.Black, ConsoleColor.White);
+            ConsoleEx.WriteAt(5, 3, "Your RATING as PRESIDENT", ConsoleColor.White, ConsoleColor.Black);
             ConsoleEx.WriteAt(1, 6, $" Total POPULARITY - {score.TotalPopularity}  ", ConsoleColor.DarkYellow, ConsoleColor.Black);
             ConsoleEx.WriteAt(1, 8, $" MONTHS in OFFICE ({governmentStats.Month}x3) - { governmentStats.Month * 3}", ConsoleColor.DarkYellow, ConsoleColor.Black);
 
             // TODO: count alive score
             ConsoleEx.WriteAt(1, 10, $" For staying alive - {score.PointsForStayingAlive}  ", ConsoleColor.DarkYellow, ConsoleColor.Black);
-            ConsoleEx.WriteAt(1, 12, $" For ");
-            ConsoleEx.WriteAt(6, 12, $"MONEYGRABBING ", ConsoleColor.Green, ConsoleColor.White);
+            ConsoleEx.WriteAt(1, 12, " For ", ConsoleColor.DarkYellow, ConsoleColor.Black);
+            ConsoleEx.WriteAt(6, 12, $"MONEYGRABBING", ConsoleColor.Green, ConsoleColor.White);
             ConsoleEx.WriteAt(6, 13, $"(${score.MoneyGrabbed}.000,000 /00,000) - {score.PointsForMoneyGrabbing}", ConsoleColor.DarkYellow, ConsoleColor.Black);
 
             int finalScore = 0;
@@ -44,8 +46,9 @@ namespace Dictator.ConsoleInterface.End
                 governmentStats.LastScore = finalScore;
             }
 
-            ConsoleEx.WriteAt(1, 16, $" Your TOTAL is {score.TotalScore}", ConsoleColor.DarkYellow, ConsoleColor.Black);
-            ConsoleEx.WriteAt(1, 18, $"[ Highest Score so far is {score.HighestScore}", ConsoleColor.DarkYellow, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 16, $" Your TOTAL is ", ConsoleColor.DarkYellow, ConsoleColor.Black);
+            ConsoleEx.Write($"{score.TotalScore}", ConsoleColor.Yellow, ConsoleColor.Black);
+            ConsoleEx.WriteAt(1, 18, $"[ Highest Score so far is {score.HighestScore} ]", ConsoleColor.DarkYellow, ConsoleColor.Black);
             pressAnyKeyControl.Show();
         }
     }
