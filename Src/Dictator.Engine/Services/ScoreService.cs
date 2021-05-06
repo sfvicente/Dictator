6dea611b821e82003a97e06f4a7df9cf380f3521
Sergio Vicente <sfvicente@gmail.com>
2021-05-07 00:34:23 +0100

    Added calculation of the score in the ScoreService to be presented in the end screen

--- Src/Dictator.Engine/Services/ScoreService.cs
+++ Src/Dictator.Engine/Services/ScoreService.cs
@@ -7,19 +7,40 @@ namespace Dictator.Core.Services
     public class ScoreService : IScoreService
     {
         private readonly IGroupStats groupStats;
+        private readonly IGovernmentStats governmentStats;
+        private readonly IAccountService accountService;
 
-        public ScoreService(IGroupStats groupStats)
+        public ScoreService(IGroupStats groupStats, IGovernmentStats governmentStats, IAccountService accountService)
         {
             this.groupStats = groupStats;
+            this.governmentStats = governmentStats;
+            this.accountService = accountService;
         }
 
         public Score GetCurrentScore()
         {
+            int totalPopularity = groupStats.GetTotalPopularity();
+            int monthsInOffice = governmentStats.Month;
+            int pointsForMonthsInOffice = monthsInOffice * 3;
+            int moneyGrabbed = accountService.GetSwissBankAccountBalance();
+            int pointsForMoneyGrabbing = moneyGrabbed / 10;
+            int highestScore = governmentStats.LastScore;
+            int pointsForStayingAlive = governmentStats.IsPlayerAlive ? 10 : 0;
+            int totalScore = totalPopularity + pointsForMonthsInOffice + pointsForMoneyGrabbing + pointsForStayingAlive;
+
             Score score = new Score()
             {
-                TotalPopularity = groupStats.GetTotalPopularity(),
-                // TODO: Complement with remaining data
+                TotalPopularity = totalPopularity,
+                MonthsInOffice = monthsInOffice,
+                PointsForMonthsInOffice = pointsForMonthsInOffice,
+                MoneyGrabbed = moneyGrabbed,
+                PointsForMoneyGrabbing = pointsForMoneyGrabbing,
+                HighestScore = highestScore,
+                PointsForStayingAlive = pointsForStayingAlive,
+                TotalScore = totalScore
             };
+
+            // TODO: save history?
 
             return score;
         }
