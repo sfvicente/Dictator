b9c3a5dafcb5d835d13e3487eeacf6442aa4c92a
Sergio Vicente <sfvicente@gmail.com>
2021-05-09 00:58:21 +0100

    Moved the GroupService classes to the Dictator.Core.Services namespace

--- Src/Dictator.Engine/Services/GroupService.cs
+++ Src/Dictator.Engine/Services/GroupService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Dictator.Core
+namespace Dictator.Core.Services
 {
     public class GroupService : IGroupService
     {
@@ -99,7 +99,7 @@ namespace Dictator.Core
 
         public int GetTotalPopularity()
         {
-            return groups.Sum(x => x.Popularity); 
+            return groups.Sum(x => x.Popularity);
         }
 
         /// <summary>
--- Src/Dictator.Engine/Services/IGroupService.cs
+++ Src/Dictator.Engine/Services/IGroupService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Dictator.Core
+namespace Dictator.Core.Services
 {
     public interface IGroupService
     {
