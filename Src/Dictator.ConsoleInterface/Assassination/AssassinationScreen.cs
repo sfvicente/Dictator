c7491270b989d8118fe18432e3878ff138075db7
Sergio Vicente <sfvicente@gmail.com>
2021-05-16 23:06:09 +0100

    Replaced the calculations on the AssassinationScreen for presenting centered text with the console extension method WriteCenteredAt()

--- Src/Dictator.ConsoleInterface/Assassination/AssassinationScreen.cs
+++ Src/Dictator.ConsoleInterface/Assassination/AssassinationScreen.cs
@@ -19,11 +19,9 @@ namespace Dictator.ConsoleInterface.Assassination
 
         public void Show(string groupName)
         {
-            int startPosition = (32 - groupName.Length) / 2;
-
             ConsoleEx.Clear();
             ConsoleEx.WriteAt(1, 11, "      ASSASSINATION ATTEMPT     ");
-            ConsoleEx.WriteAt(startPosition, 12, $"by one of {groupName}");
+            ConsoleEx.WriteCenteredAt(12, $"by one of {groupName}");
             pressAnyKeyControl.Show();
         }
     }
