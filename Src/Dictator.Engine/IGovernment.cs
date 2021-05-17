db69c6c4a47fe17a0300f383f8c344dc35555d2b
Sergio Vicente <sfvicente@gmail.com>
2021-05-17 07:19:15 +0100

    Added documentation to the Government class and IGovernment interface

--- Src/Dictator.Engine/IGovernment.cs
+++ Src/Dictator.Engine/IGovernment.cs
@@ -6,13 +6,44 @@ namespace Dictator.Core
 {
     public interface IGovernment
     {
+        /// <summary>
+        ///     Gets a value indicating whether the player is currently alive.
+        /// </summary>
         public bool IsPlayerAlive { get; set; }
+
+        /// <summary>
+        ///     Gets a value indicating whether the player has acquired an helicopter.
+        /// </summary>
         public bool HasHelicopter { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the player strength. 
+        /// </summary>
         public int PlayerStrength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the current month of government. 
+        /// </summary>
         public int Month { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the plot bonus. 
+        /// </summary>
         public int PlotBonus { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the monthly revolution strength. 
+        /// </summary>
         public int MonthlyRevolutionStrength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the monthly minimal popularity and strength requirement.. 
+        /// </summary>
         public int MonthlyMinimalPopularityAndStrength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the current high score. 
+        /// </summary>
         public int LastScore { get; set; }
     }
 }
