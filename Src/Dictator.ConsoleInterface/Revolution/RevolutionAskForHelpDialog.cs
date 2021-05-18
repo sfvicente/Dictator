0e54fee57394f7d51181cb877632a83f468f9263
Sergio Vicente <sfvicente@gmail.com>
2021-05-18 22:57:23 +0100

    Added first version of the mechanism to display the possible allies in the RevolutionAskForHelpDialog

--- Src/Dictator.ConsoleInterface/Revolution/RevolutionAskForHelpDialog.cs
+++ Src/Dictator.ConsoleInterface/Revolution/RevolutionAskForHelpDialog.cs
@@ -1,4 +1,5 @@
 ﻿using Dictator.Common;
+using Dictator.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +8,7 @@ namespace Dictator.ConsoleInterface.Revolution
 {
     public class RevolutionAskForHelpDialog : IRevolutionAskForHelpDialog
     {
-        public int Show()
+        public int Show(Dictionary<int, Group> possibleAllies)
         {
             ConsoleEx.Clear(ConsoleColor.Gray, ConsoleColor.Black);
             ConsoleEx.WriteAt(1, 12, "           REVOLUTION           ");
@@ -15,6 +16,24 @@ namespace Dictator.ConsoleInterface.Revolution
             // TODO: add groups and allies
 
             ConsoleEx.WriteAt(1, 9, "  WHO are you ASKING for HELP ? ");
+
+            int line = 12;
+
+            for (int i = 1; i < 7; i++)
+            {
+                if(possibleAllies.ContainsKey(i))
+                {
+                    ConsoleEx.WriteAt(line, 6, $"{i} {possibleAllies.GetValueOrDefault(i)}");
+                }
+                else
+                {
+                    ConsoleEx.WriteEmptyLineAt(line);
+                }
+
+                line++;
+            }
+
+            // TODO: detect number pressed
 
             return 0;
         }
