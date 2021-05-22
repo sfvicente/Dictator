1ceb787ea946c96f163c7414543819596f6cf9cb
Sergio Vicente <sfvicente@gmail.com>
2021-05-22 01:36:04 +0100

    Added scaffold class for the NewsService tests

---
+++ Src/Dictator.Tests/Services/NewsServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Dictator.Tests
+{
+    public class NewsServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
