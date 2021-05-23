a9956dbba34463a8741bf26f8f2b0343a36be6a0
Sergio Vicente <sfvicente@gmail.com>
2021-05-23 22:44:14 +0100

    Added scaffold class for the loan service tests

---
+++ Src/Dictator.Tests/Services/LoanServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Dictator.Tests
+{
+    public class LoanServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
