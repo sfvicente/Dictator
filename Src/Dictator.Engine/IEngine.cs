757bbe379cded5a69ae70ce64dba38ac00f26fed
Sergio Vicente <sfvicente@gmail.com>
2021-07-20 22:30:16 +0100

    Added missing comment for the ShouldWarHappen() method signature in the IEngine interface

--- Src/Dictator.Engine/IEngine.cs
+++ Src/Dictator.Engine/IEngine.cs
@@ -146,7 +146,13 @@ namespace Dictator.Core
         public string GetAssassinationGroupName();
         public bool IsAssassinationSuccessful();
         public bool DoesConflictExist();
+
+        /// <summary>
+        ///     Determines if a war between the Ritimba republic and Leftoto should happen. There is a 1/3 probability that war should happen.
+        /// </summary>
+        /// <returns><c>true</c> if war should happen; otherwise, <c>false</c>.</returns>
         public bool ShouldWarHappen();
+        
         public void ApplyThreatOfWarEffects();
         public WarStats BeginInvasion();
         public bool ExecuteWar(WarStats warStats);
