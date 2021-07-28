cacef75cd8625e13539bbda417fa01effae706ec
Sergio Vicente <sfvicente@gmail.com>
2021-07-28 23:23:56 +0100

    Add documentation to all the members of the Account classes

--- Src/Dictator.Engine/IAccount.cs
+++ Src/Dictator.Engine/IAccount.cs
@@ -6,9 +6,24 @@ namespace Dictator.Core
 {
     public interface IAccount
     {
+        /// <summary>
+        ///     Gets or sets the balance of the treasury.
+        /// </summary>
         public int TreasuryBalance { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the monthly costs for the treasury.
+        /// </summary>
         public int MonthlyCosts { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value that indicates whether the player has a Swiss bank account.
+        /// </summary>
         public bool HasSwissBankAccount { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the balance of the Swiss bank account.
+        /// </summary>
         public int SwissBankAccountBalance { get; set; }
     }
 }
