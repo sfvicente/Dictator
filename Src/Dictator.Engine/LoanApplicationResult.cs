0713aeb45d2136e410615e339ab8cc66048906d3
Sergio Vicente <sfvicente@gmail.com>
2021-07-31 22:14:36 +0100

    Removed unnecessary using statements from the LoadApplicationResult class file

--- Src/Dictator.Engine/LoanApplicationResult.cs
+++ Src/Dictator.Engine/LoanApplicationResult.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dictator.Core
+﻿namespace Dictator.Core
 {
     /// <summary>
     ///     Represents the outcome of a loan application.
