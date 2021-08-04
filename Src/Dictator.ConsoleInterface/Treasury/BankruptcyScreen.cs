6ed5744888fce472c7c830aa529d89b0b6f458ac
Sergio Vicente <sfvicente@gmail.com>
2021-08-04 22:53:59 +0100

    Removed unncessary using statements from the BankruptcyScreen types

--- Src/Dictator.ConsoleInterface/Treasury/BankruptcyScreen.cs
+++ Src/Dictator.ConsoleInterface/Treasury/BankruptcyScreen.cs
@@ -1,9 +1,5 @@
 ﻿using Dictator.Common;
 using Dictator.ConsoleInterface.Common;
-using Dictator.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Dictator.ConsoleInterface.Treasury
 {
