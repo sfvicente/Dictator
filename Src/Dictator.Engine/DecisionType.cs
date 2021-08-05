090c1fc150558cb7a1638a8c9cab4ae661bba515
Sergio Vicente <sfvicente@gmail.com>
2021-08-05 22:59:59 +0100

    Removed unnecessary using statements from the DecisionType enumeration file

--- Src/Dictator.Engine/DecisionType.cs
+++ Src/Dictator.Engine/DecisionType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dictator.Core
+﻿namespace Dictator.Core
 {
     /// <summary>
     ///     Specifies the type of a presidential decision.
