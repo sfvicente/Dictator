8e542ba74ca2b1eef5646e5117433fb11be3427d
Sergio Vicente <sfvicente@gmail.com>
2021-08-06 21:35:20 +0100

    Commented the GroupType enum

--- Src/Dictator.Engine/GroupType.cs
+++ Src/Dictator.Engine/GroupType.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Dictator.Core
 {
+    /// <summary>
+    ///     Specifies the type of a group.
+    /// </summary>
     public enum GroupType
     {
         Army = 0,
