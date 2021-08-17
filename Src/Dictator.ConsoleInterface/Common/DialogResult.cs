efa1b6b48e0b0631f62d08ab81bef5a7f6de07ce
Sergio Vicente <sfvicente@gmail.com>
2021-08-17 22:48:11 +0100

    Commented the DialogResult enumeration

--- Src/Dictator.ConsoleInterface/Common/DialogResult.cs
+++ Src/Dictator.ConsoleInterface/Common/DialogResult.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Dictator.ConsoleInterface.Common
 {
+    /// <summary>
+    ///     Specifies the chosen action after the dialog was presented.
+    /// </summary>
     public enum DialogResult
     {
         Yes = 0,
