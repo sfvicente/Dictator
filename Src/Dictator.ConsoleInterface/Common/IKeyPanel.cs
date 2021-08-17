1463a6e20f42a7e0be3799d817dc19b464bb952d
Sergio Vicente <sfvicente@gmail.com>
2021-08-17 22:50:09 +0100

    Removed unnecessary using statements in the IKeyPanel type file

--- Src/Dictator.ConsoleInterface/Common/IKeyPanel.cs
+++ Src/Dictator.ConsoleInterface/Common/IKeyPanel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dictator.ConsoleInterface.Common
+﻿namespace Dictator.ConsoleInterface.Common
 {
     public interface IKeyPanel
     {
