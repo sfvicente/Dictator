f2895ea31ab94cea26584597f969186688aa35a1
Sergio Vicente <sfvicente@gmail.com>
2021-08-19 23:38:49 +0100

    Documented the Show() method in the EscapeAttemptDialog types

--- Src/Dictator.ConsoleInterface/Escape/IEscapeAttemptDialog.cs
+++ Src/Dictator.ConsoleInterface/Escape/IEscapeAttemptDialog.cs
@@ -4,6 +4,10 @@ namespace Dictator.ConsoleInterface.Escape
 {
     public interface IEscapeAttemptDialog
     {
+        /// <summary>
+        ///     Displays the dialog.
+        /// </summary>
+        /// <returns>The option selected after the dialog has been presented.</returns>
         DialogResult Show();
     }
 }
