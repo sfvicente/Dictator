87fc84d29657d0926051f2c9623f8826249f2b90
Sergio Vicente <sfvicente@gmail.com>
2021-08-20 23:38:43 +0100

    Commented the Country enum

--- Src/Dictator.Engine/Country.cs
+++ Src/Dictator.Engine/Country.cs
@@ -1,8 +1,11 @@
 ﻿namespace Dictator.Core
 {
+    /// <summary>
+    ///     Specifies the country of the lender of the foreign aid.
+    /// </summary>
     public enum Country
     {
         America = 1,
         Russia = 2
     }
-}
+}n
