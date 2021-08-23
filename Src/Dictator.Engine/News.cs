10a8201026931d47ef6627a1f382b4ebeff0a207
Sergio Vicente <sfvicente@gmail.com>
2021-08-23 23:18:32 +0100

    Commented the News class

--- Src/Dictator.Engine/News.cs
+++ Src/Dictator.Engine/News.cs
@@ -1,5 +1,8 @@
 ﻿namespace Dictator.Core
 {
+    /// <summary>
+    ///     Represents a news event in the game that happens outside of the player control.
+    /// </summary>
     public class News : GameAction
     {
         public News(int cost, int monthlyCost, string groupPopularityChanges, string groupStrenghtChanges, string text)
