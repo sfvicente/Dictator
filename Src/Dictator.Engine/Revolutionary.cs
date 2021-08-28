35e326732646444582175a245e7f2338eac3ca42
Sergio Vicente <sfvicente@gmail.com>
2021-08-28 22:57:35 +0100

    Added the Revolutionary class to be able to supply the required information to the AskForHelpDialog screen

---
+++ Src/Dictator.Engine/Revolutionary.cs
@@ -0,0 +1,20 @@
+﻿namespace Dictator.Core
+{
+    public class Revolutionary
+    {
+        /// <summary>
+        ///     Gets or sets the revolutionary group name.
+        /// </summary>
+        public string RevolutionaryGroupName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the revolutionary group ally name.
+        /// </summary>
+        public string RevolutionaryGroupAllyName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the combined strength of the revolutionary group and their ally.
+        /// </summary>
+        public int CombinedStrength { get; set; }
+    }
+}
