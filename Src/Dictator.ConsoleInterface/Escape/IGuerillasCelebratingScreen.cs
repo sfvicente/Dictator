c11ebd03c935f3943e77d6abfb989becf140300e
Sergio Vicente <sfvicente@gmail.com>
2021-09-04 16:46:21 +0100

    Commented the Show() method in the IGuerillasCelebratingScreen interface and GuerillasCelebratingScreen class

--- Src/Dictator.ConsoleInterface/Escape/IGuerillasCelebratingScreen.cs
+++ Src/Dictator.ConsoleInterface/Escape/IGuerillasCelebratingScreen.cs
@@ -6,6 +6,9 @@ namespace Dictator.ConsoleInterface.Escape
 {
     public interface IGuerillasCelebratingScreen
     {
+        /// <summary>
+        ///     Displays the screen.
+        /// </summary>
         public void Show();
     }
 }
