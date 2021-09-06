da077097001281c2182c94f7b33ec88335235632
Sergio Vicente <sfvicente@gmail.com>
2021-09-06 20:59:07 +0100

    Commented the Show() method in the IHelicopterWontStartScreen interface and HelicopterWontStartScreen class

--- Src/Dictator.ConsoleInterface/Escape/IHelicopterWontStartScreen.cs
+++ Src/Dictator.ConsoleInterface/Escape/IHelicopterWontStartScreen.cs
@@ -6,6 +6,9 @@ namespace Dictator.ConsoleInterface.Escape
 {
     public interface IHelicopterWontStartScreen
     {
+        /// <summary>
+        ///     Displays the screen
+        /// </summary>
         public void Show();
     }
 }
