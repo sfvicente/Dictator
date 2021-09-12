5dc4098349923655e9c1fccb79667c72b02199af
Sergio Vicente <sfvicente@gmail.com>
2021-09-12 18:25:47 +0100

    Commented the IPressAnyKeyOrOptionControl interface and the PressAnyKeyOrOptionControl class

--- Src/Dictator.ConsoleInterface/Common/PressAnyKeyOrOptionControl.cs
+++ Src/Dictator.ConsoleInterface/Common/PressAnyKeyOrOptionControl.cs
@@ -2,6 +2,10 @@
 
 namespace Dictator.ConsoleInterface.Common
 {
+    /// <summary>
+    ///     Represents the control that is displayed with a panel when the user is required
+    ///     to press a key or select an option.
+    /// </summary>
     public class PressAnyKeyOrOptionControl : IPressAnyKeyOrOptionControl
     {
         private readonly IKeyPanel keyPanel;
