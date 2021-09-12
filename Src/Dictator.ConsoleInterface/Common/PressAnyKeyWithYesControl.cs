8e17ea38191d0c04442cfdf8cbbcb39e0459a6cf
Sergio Vicente <sfvicente@gmail.com>
2021-09-12 18:24:57 +0100

    Documented the IPressAnyKeyWithYesControl interface and the PressAnyKeyWithYesControl class

--- Src/Dictator.ConsoleInterface/Common/PressAnyKeyWithYesControl.cs
+++ Src/Dictator.ConsoleInterface/Common/PressAnyKeyWithYesControl.cs
@@ -3,6 +3,10 @@ using System;
 
 namespace Dictator.ConsoleInterface.Common
 {
+    /// <summary>
+    ///     Represents the control that is displayed with a panel when the user is required
+    ///     to press a key or select yes.
+    /// </summary>
     public class PressAnyKeyWithYesControl : IPressAnyKeyWithYesControl
     {
         private readonly IKeyPanel keyPanel;
