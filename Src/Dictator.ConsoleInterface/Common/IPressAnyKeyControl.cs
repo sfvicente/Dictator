3cbce796338a97108f9a316ac95d345ceeb7a9cd
Sergio Vicente <sfvicente@gmail.com>
2021-09-12 18:23:32 +0100

    Documented the IPressAnyKeyControl interface and the PressAnyKeyControl class

--- Src/Dictator.ConsoleInterface/Common/IPressAnyKeyControl.cs
+++ Src/Dictator.ConsoleInterface/Common/IPressAnyKeyControl.cs
@@ -1,5 +1,9 @@
 ﻿namespace Dictator.ConsoleInterface.Common
 {
+    /// <summary>
+    ///     Represents the control that is displayed with a panel when the user is required
+    ///     to press a key.
+    /// </summary>
     public interface IPressAnyKeyControl
     {
         /// <summary>
