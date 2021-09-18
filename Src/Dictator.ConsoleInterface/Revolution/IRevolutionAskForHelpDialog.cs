071a9f2b5d68bfb1408113066cd0c6850ce9124a
Sergio Vicente <sfvicente@gmail.com>
2021-09-18 16:47:49 +0100

    Commented the IRevolutionAskForHelpDialog interface and the RevolutionAskForHelpDialog class

--- Src/Dictator.ConsoleInterface/Revolution/IRevolutionAskForHelpDialog.cs
+++ Src/Dictator.ConsoleInterface/Revolution/IRevolutionAskForHelpDialog.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace Dictator.ConsoleInterface.Revolution
 {
+    /// <summary>
+    ///     Represents the dialog that is displayed when the player must choose a group
+    ///     to form an alliance in a revolution scenario.
+    /// </summary>
     public interface IRevolutionAskForHelpDialog
     {
         int Show(Revolutionary revolutionary, Dictionary<int, Group> possibleAllies);
--- Src/Dictator.ConsoleInterface/Revolution/RevolutionAskForHelpDialog.cs
+++ Src/Dictator.ConsoleInterface/Revolution/RevolutionAskForHelpDialog.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 
 namespace Dictator.ConsoleInterface.Revolution
 {
+    /// <summary>
+    ///     Represents the dialog that is displayed when the player must choose a group
+    ///     to form an alliance in a revolution scenario.
+    /// </summary>
     public class RevolutionAskForHelpDialog : IRevolutionAskForHelpDialog
     {
         public int Show(Revolutionary revolutionary, Dictionary<int, Group> possibleAllies)
