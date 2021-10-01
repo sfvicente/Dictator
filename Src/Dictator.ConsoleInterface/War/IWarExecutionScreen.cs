b234fdbdbc932fce610e799526a33b9f94608d61
Sergio Vicente <sfvicente@gmail.com>
2021-10-01 22:36:33 +0100

    Commented the IWarExecutionScreen interface and the WarExecutionScreen class

--- Src/Dictator.ConsoleInterface/War/IWarExecutionScreen.cs
+++ Src/Dictator.ConsoleInterface/War/IWarExecutionScreen.cs
@@ -1,5 +1,9 @@
 ﻿namespace Dictator.ConsoleInterface.War
 {
+    /// <summary>
+    ///     Represents the screen that is displayed when a player loses the war, is unable to
+    ///     escape by helicopter and is executed.
+    /// </summary>
     public interface IWarExecutionScreen
     {
         /// <summary>
--- Src/Dictator.ConsoleInterface/War/WarExecutionScreen.cs
+++ Src/Dictator.ConsoleInterface/War/WarExecutionScreen.cs
@@ -4,6 +4,10 @@ using System;
 
 namespace Dictator.ConsoleInterface.War
 {
+    /// <summary>
+    ///     Represents the screen that is displayed when a player loses the war, is unable to
+    ///     escape by helicopter and is executed.
+    /// </summary>
     public class WarExecutionScreen : IWarExecutionScreen
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
