4c92a66a8e206da8c406ef38555036e1cdd5405b
Sergio Vicente <sfvicente@gmail.com>
2021-10-03 20:53:36 +0100

    Documented the IWarLeftotoInvadesScreen interface and the WarLeftotoInvadesScreen class

--- Src/Dictator.ConsoleInterface/War/IWarLeftotoInvadesScreen.cs
+++ Src/Dictator.ConsoleInterface/War/IWarLeftotoInvadesScreen.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Dictator.ConsoleInterface.War
 {
+    /// <summary>
+    ///     Represents the screen that is displayed when Leftoto invades and initiates the war
+    ///     against the republic of Ritimban.
+    /// </summary>
     public interface IWarLeftotoInvadesScreen
     {
         /// <summary>
--- Src/Dictator.ConsoleInterface/War/WarLeftotoInvadesScreen.cs
+++ Src/Dictator.ConsoleInterface/War/WarLeftotoInvadesScreen.cs
@@ -7,6 +7,10 @@ using System.Text;
 
 namespace Dictator.ConsoleInterface.War
 {
+    /// <summary>
+    ///     Represents the screen that is displayed when Leftoto invades and initiates the war
+    ///     against the republic of Ritimban.
+    /// </summary>
     public class WarLeftotoInvadesScreen : IWarLeftotoInvadesScreen
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
