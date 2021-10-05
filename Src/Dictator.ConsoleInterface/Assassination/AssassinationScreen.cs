cc7a1486ca1a790e0d13143ae63ec59d82866953
Sergio Vicente <sfvicente@gmail.com>
2021-10-05 23:21:29 +0100

    Documented the IAssassinationScreen interface and the AssassinationScreen class

--- Src/Dictator.ConsoleInterface/Assassination/AssassinationScreen.cs
+++ Src/Dictator.ConsoleInterface/Assassination/AssassinationScreen.cs
@@ -8,6 +8,10 @@ using System.Text;
 
 namespace Dictator.ConsoleInterface.Assassination
 {
+    /// <summary>
+    ///     Represents the screen that is displayed when an assassination attempt by one of the groups 
+    ///     against the player happens.
+    /// </summary>
     public class AssassinationScreen : IAssassinationScreen
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
--- Src/Dictator.ConsoleInterface/Assassination/IAssassinationScreen.cs
+++ Src/Dictator.ConsoleInterface/Assassination/IAssassinationScreen.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Dictator.ConsoleInterface.Assassination
 {
+    /// <summary>
+    ///     Represents the screen that is displayed when an assassination attempt by one of the groups 
+    ///     against the player happens.
+    /// </summary>
     public interface IAssassinationScreen
     {
         public void Show(string groupName);
