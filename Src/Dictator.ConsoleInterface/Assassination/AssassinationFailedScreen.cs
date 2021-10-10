e14bf15435db65d57e5f042f43cc26f5b0778a11
Sergio Vicente <sfvicente@gmail.com>
2021-10-10 23:23:59 +0100

    Commented the AssassinationFailedScreen class constructor

--- Src/Dictator.ConsoleInterface/Assassination/AssassinationFailedScreen.cs
+++ Src/Dictator.ConsoleInterface/Assassination/AssassinationFailedScreen.cs
@@ -12,6 +12,11 @@ namespace Dictator.ConsoleInterface.Assassination
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AssassinationFailedScreen"/> class from a <see cref="IPressAnyKeyControl"/>
+        ///     component.
+        /// </summary>
+        /// <param name="pressAnyKeyControl">The control that is displayed when the user is required to press a key.</param>
         public AssassinationFailedScreen(IPressAnyKeyControl pressAnyKeyControl)
         {
             this.pressAnyKeyControl = pressAnyKeyControl;
