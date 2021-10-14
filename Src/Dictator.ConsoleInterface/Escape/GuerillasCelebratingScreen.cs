46a13a833e9363b2e7bf65f2ba4a452fc97dbecd
Sergio Vicente <sfvicente@gmail.com>
2021-10-14 21:53:54 +0100

    Commented the GuerillasCelebratingScreen class constructor

--- Src/Dictator.ConsoleInterface/Escape/GuerillasCelebratingScreen.cs
+++ Src/Dictator.ConsoleInterface/Escape/GuerillasCelebratingScreen.cs
@@ -8,6 +8,11 @@ namespace Dictator.ConsoleInterface.Escape
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GuerillasCelebratingScreen"/> class from a <see cref="IPressAnyKeyControl"/>
+        ///     component.
+        /// </summary>
+        /// <param name="pressAnyKeyControl">The control that is displayed when the user is required to press a key.</param>
         public GuerillasCelebratingScreen(IPressAnyKeyControl pressAnyKeyControl)
         {
             this.pressAnyKeyControl = pressAnyKeyControl;
