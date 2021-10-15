554e016f17126cf7817f0cf2eac66fdbf2dcef90
Sergio Vicente <sfvicente@gmail.com>
2021-10-15 11:08:33 +0100

    Commented the HelicopterEscapeScreen class constructor

--- Src/Dictator.ConsoleInterface/Escape/HelicopterEscapeScreen.cs
+++ Src/Dictator.ConsoleInterface/Escape/HelicopterEscapeScreen.cs
@@ -11,6 +11,11 @@ namespace Dictator.ConsoleInterface.Escape
     {
         private readonly IPressAnyKeyControl pressAnyKeyControl;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="HelicopterEscapeScreen"/> class from a <see cref="IPressAnyKeyControl"/>
+        ///     component.
+        /// </summary>
+        /// <param name="pressAnyKeyControl">The control that is displayed when the user is required to press a key.</param>
         public HelicopterEscapeScreen(IPressAnyKeyControl pressAnyKeyControl)
         {
             this.pressAnyKeyControl = pressAnyKeyControl;
