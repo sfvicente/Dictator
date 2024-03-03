fb53761bbf1e4d42c38870032ae04523666cbbba
Sergio Vicente <sfvicente@gmail.com>
2024-03-03 20:34:09 +0000

    Implemented a random service class to remove class dependencies on the Random class and allow for easier unit testing

---
+++ Src/Dictator.Engine/Services/RandomService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dictator.Core.Services;
+
+public interface IRandomService
+{
+    int Next(int minValue, int maxValue);
+}
+
+public class RandomService : IRandomService
+{
+    private readonly Random _random;
+
+    public RandomService()
+    {
+        _random = new Random();
+    }
+
+    public int Next(int minValue, int maxValue)
+    {
+        return _random.Next(minValue, maxValue);
+    }
+}
