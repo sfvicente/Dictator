2e7330de7925d76da1bdc8241fef5cad1a74f77d
Sergio Vicente <sfvicente@gmail.com>
2024-03-10 11:49:51 +0000

    Cleaned up and simplified the code in the ReportService file to improve readability

--- Src/Dictator.Engine/Services/ReportService.cs
+++ Src/Dictator.Engine/Services/ReportService.cs
@@ -2,32 +2,17 @@
 
 namespace Dictator.Core.Services;
 
-/// <summary>
-///     Provides functionality related to reporting within the game.
-/// </summary>
 public interface IReportService
 {
-    /// <summary>
-    ///     Retrieves a request for a detailed account or statement from police on the state of the groups and government.
-    /// </summary>
-    /// <returns>A police report request with information on the popularity and strength requirements.</returns>
-    public PoliceReportRequest RequestPoliceReport();
-
-    /// <summary>
-    ///     Retrieves the police report with the current month, revolution strength, player strength and group information.
-    /// </summary>
-    /// <returns>The police report with the current month, revolution strength, player strength and group information.</returns>
-    public PoliceReport GetPoliceReport();
+    PoliceReportRequest RequestPoliceReport();
+    PoliceReport GetPoliceReport();
 }
 
-/// <summary>
-///     Provides functionality related to reporting within the game.
-/// </summary>
 public class ReportService : IReportService
 {
-    private readonly IAccountService accountService;
-    private readonly IGroupService groupService;
-    private readonly IGovernmentService governmentService;
+    private readonly IAccountService _accountService;
+    private readonly IGroupService _groupService;
+    private readonly IGovernmentService _governmentService;
 
     /// <summary>
     ///     Initializes a new instance of the <see cref="ReportService"/> class from a <see cref="IAccountService"/>,
@@ -39,9 +24,9 @@ public class ReportService : IReportService
     /// <param name="governmentService">The service used to provide functionality related to the government settings and operations.</param>
     public ReportService(IAccountService accountService, IGroupService groupService, IGovernmentService governmentService)
     {
-        this.accountService = accountService;
-        this.groupService = groupService;
-        this.governmentService = governmentService;
+        _accountService = accountService;
+        _groupService = groupService;
+        _governmentService = governmentService;
     }
 
     /// <summary>
@@ -50,13 +35,13 @@ public class ReportService : IReportService
     /// <returns>A police report request with information on the popularity and strength requirements.</returns>
     public PoliceReportRequest RequestPoliceReport()
     {
-        PoliceReportRequest policeReportRequest = new PoliceReportRequest
+        PoliceReportRequest policeReportRequest = new()
         {
-            HasEnoughBalance = accountService.GetTreasuryBalance() > 0,
+            HasEnoughBalance = _accountService.GetTreasuryBalance() > 0,
             IsPlayerPopularWithSecretPolice = IsPlayerPopularWithSecretPolice(),
             HasPoliceEnoughStrength = HasPoliceEnoughStrength(),
-            PolicePopularity = groupService.GetPopularityByGroupType(GroupType.SecretPolice),
-            PoliceStrength = groupService.GetStrengthByGroupType(GroupType.SecretPolice)
+            PolicePopularity = _groupService.GetPopularityByGroupType(GroupType.SecretPolice),
+            PoliceStrength = _groupService.GetStrengthByGroupType(GroupType.SecretPolice)
         };
 
         return policeReportRequest;
@@ -68,12 +53,12 @@ public class ReportService : IReportService
     /// <returns>The police report with the current month, revolution strength, player strength and group information.</returns>
     public PoliceReport GetPoliceReport()
     {
-        PoliceReport policeReport = new PoliceReport
+        PoliceReport policeReport = new()
         {
-            Month = governmentService.GetMonth(),
-            Groups = groupService.GetGroups().AsReadOnly(),
-            PlayerStrength = governmentService.GetPlayerStrength(),
-            MonthlyRevolutionStrength = governmentService.GetMonthlyRevolutionStrength()
+            Month = _governmentService.GetMonth(),
+            Groups = _groupService.GetGroups().AsReadOnly(),
+            PlayerStrength = _governmentService.GetPlayerStrength(),
+            MonthlyRevolutionStrength = _governmentService.GetMonthlyRevolutionStrength()
         };
 
         return policeReport;
@@ -85,9 +70,9 @@ public class ReportService : IReportService
     /// <returns><c>true</c> if the player is popular with the secret police; otherwise, <c>false</c>.</returns>
     private bool IsPlayerPopularWithSecretPolice()
     {
-        int secretPolicePopularity = groupService.GetPopularityByGroupType(GroupType.SecretPolice);
+        int secretPolicePopularity = _groupService.GetPopularityByGroupType(GroupType.SecretPolice);
 
-        return secretPolicePopularity > governmentService.GetMonthlyMinimalPopularityAndStrength();
+        return secretPolicePopularity > _governmentService.GetMonthlyMinimalPopularityAndStrength();
     }
 
     /// <summary>
@@ -96,8 +81,8 @@ public class ReportService : IReportService
     /// <returns><c>true</c> if police has enough; otherwise, <c>false</c>.</returns>
     private bool HasPoliceEnoughStrength()
     {
-        int policeStrength = groupService.GetStrengthByGroupType(GroupType.SecretPolice);
+        int policeStrength = _groupService.GetStrengthByGroupType(GroupType.SecretPolice);
 
-        return policeStrength > governmentService.GetMonthlyMinimalPopularityAndStrength();
+        return policeStrength > _governmentService.GetMonthlyMinimalPopularityAndStrength();
     }
 }
