bdcfd96effc6487556484d2948f300f2d491e813
Sergio Vicente <sfvicente@gmail.com>
2024-03-15 23:04:00 +0000

    Added unit tests for the RandomService class

---
+++ Src/Dictator.Tests/Services/RandomServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Dictator.Core.Services;
+using NUnit.Framework;
+
+namespace Dictator.Tests.Services;
+
+[TestFixture]
+public class RandomServiceTests
+{
+    private IRandomService _randomService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _randomService = new RandomService();
+    }
+
+    [Test]
+    public void Next_MaxValue_ReturnsValueLessThanMax()
+    {
+        int maxValue = 100;
+        int result = _randomService.Next(maxValue);
+        Assert.That(result, Is.LessThan(maxValue));
+    }
+
+    [Test]
+    public void Next_MinMaxValues_ReturnsValueInRange()
+    {
+        int minValue = 50;
+        int maxValue = 100;
+        int result = _randomService.Next(minValue, maxValue);
+        Assert.That(result, Is.GreaterThanOrEqualTo(minValue));
+        Assert.That(result, Is.LessThan(maxValue));
+    }
+}
