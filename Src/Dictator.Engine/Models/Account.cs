e1b348de894a9640851d37c83e6e8ad82db3d555
Sergio Vicente <sfvicente@gmail.com>
2024-03-15 23:36:59 +0000

    Modified Account.cs file to use file-scoped namespace

--- Src/Dictator.Engine/Models/Account.cs
+++ Src/Dictator.Engine/Models/Account.cs
@@ -1,29 +1,28 @@
-﻿namespace Dictator.Core.Models
+﻿namespace Dictator.Core.Models;
+
+/// <summary>
+///     Represents the account information including Swiss bank account information, treasury balance
+///     and monthly costs.
+/// </summary>
+public class Account
 {
     /// <summary>
-    ///     Represents the account information including Swiss bank account information, treasury balance
-    ///     and monthly costs.
+    ///     Gets or sets the balance of the treasury.
     /// </summary>
-    public class Account
-    {
-        /// <summary>
-        ///     Gets or sets the balance of the treasury.
-        /// </summary>
-        public int TreasuryBalance { get; set; }
+    public int TreasuryBalance { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the monthly costs for the treasury.
-        /// </summary>
-        public int MonthlyCosts { get; set; }
+    /// <summary>
+    ///     Gets or sets the monthly costs for the treasury.
+    /// </summary>
+    public int MonthlyCosts { get; set; }
 
-        /// <summary>
-        ///     Gets or sets a value that indicates whether the player has a Swiss bank account.
-        /// </summary>
-        public bool HasSwissBankAccount { get; set; }
+    /// <summary>
+    ///     Gets or sets a value that indicates whether the player has a Swiss bank account.
+    /// </summary>
+    public bool HasSwissBankAccount { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the balance of the Swiss bank account.
-        /// </summary>
-        public int SwissBankAccountBalance { get; set; }
-    }
+    /// <summary>
+    ///     Gets or sets the balance of the Swiss bank account.
+    /// </summary>
+    public int SwissBankAccountBalance { get; set; }
 }
