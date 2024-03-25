b7cdd63a6a64a9c12835748f9ed59f6bb1ec654b
Sergio Vicente <sfvicente@gmail.com>
2024-03-25 23:56:29 +0000

    Converted the MonthScreen.cs file to use file-scoped namespaces

--- Src/Dictator.ConsoleInterface/Events/MonthScreen.cs
+++ Src/Dictator.ConsoleInterface/Events/MonthScreen.cs
@@ -1,42 +1,41 @@
 ﻿using System;
 
-namespace Dictator.ConsoleInterface.Events
+namespace Dictator.ConsoleInterface.Events;
+
+/// <summary>
+///     Represents the screen that is displayed when a new month starts, which
+///     marks a new game turn.
+/// </summary>
+public interface IMonthScreen
 {
     /// <summary>
-    ///     Represents the screen that is displayed when a new month starts, which
-    ///     marks a new game turn.
+    ///     Displays the screen.
     /// </summary>
-    public interface IMonthScreen
+    /// <param name="month">The number of the month to display on the screen.</param>
+    public void Show(int month);
+}
+
+/// <summary>
+///     Represents the screen that is displayed when a new month starts, which
+///     marks a new game turn.
+/// </summary>
+public class MonthScreen : BaseScreen, IMonthScreen
+{
+    public MonthScreen(IConsoleService console)
+        : base(console)
     {
-        /// <summary>
-        ///     Displays the screen.
-        /// </summary>
-        /// <param name="month">The number of the month to display on the screen.</param>
-        public void Show(int month);
+
     }
 
     /// <summary>
-    ///     Represents the screen that is displayed when a new month starts, which
-    ///     marks a new game turn.
+    ///     Displays the screen.
     /// </summary>
-    public class MonthScreen : BaseScreen, IMonthScreen
+    /// <param name="month">The number of the month to display on the screen.</param>
+    public void Show(int month)
     {
-        public MonthScreen(IConsoleService console)
-            : base(console)
-        {
-
-        }
-
-        /// <summary>
-        ///     Displays the screen.
-        /// </summary>
-        /// <param name="month">The number of the month to display on the screen.</param>
-        public void Show(int month)
-        {
-            _consoleService.Clear(ConsoleColor.Yellow);
-            _consoleService.WriteAt(8, 10, "MONTH ", ConsoleColor.Cyan, ConsoleColor.Black);
-            _consoleService.WriteAt(14, 10, $"{month}", ConsoleColor.White, ConsoleColor.Black);
-            Console.ReadKey(true);
-        }
+        _consoleService.Clear(ConsoleColor.Yellow);
+        _consoleService.WriteAt(8, 10, "MONTH ", ConsoleColor.Cyan, ConsoleColor.Black);
+        _consoleService.WriteAt(14, 10, $"{month}", ConsoleColor.White, ConsoleColor.Black);
+        Console.ReadKey(true);
     }
 }
