f60e356ca1c43465645fc32ece678bcc66d96c63
Sergio Vicente <sfvicente@gmail.com>
2024-03-26 00:22:28 +0000

    Added unit tests for the classes in the Events folder of the ConsoleInterface project

---
+++ Src/Dictator.ConsoleInterfaces.Tests/Events/MonthScreenTests.cs
@@ -0,0 +1,34 @@
+﻿using Dictator.ConsoleInterface.Events;
+using Dictator.ConsoleInterface;
+
+namespace Dictator.ConsoleInterfaces.Tests.Events;
+
+[TestFixture]
+public class MonthScreenTests
+{
+    private MonthScreen _monthScreen;
+    private Mock<IConsoleService> _consoleServiceMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _consoleServiceMock = new Mock<IConsoleService>();
+        _monthScreen = new MonthScreen(_consoleServiceMock.Object);
+    }
+
+    [Test]
+    public void Show_ClearsConsoleAndWritesMonth()
+    {
+        // Arrange
+        int month = 1;
+
+        // Act
+        _monthScreen.Show(month);
+
+        // Assert
+        _consoleServiceMock.Verify(cs => cs.Clear(ConsoleColor.Yellow), Times.Once);
+        _consoleServiceMock.Verify(cs => cs.WriteAt(8, 10, "MONTH ", ConsoleColor.Cyan, ConsoleColor.Black), Times.Once);
+        _consoleServiceMock.Verify(cs => cs.WriteAt(14, 10, $"{month}", ConsoleColor.White, ConsoleColor.Black), Times.Once);
+    }
+}
+
---
+++ Src/Dictator.ConsoleInterfaces.Tests/Events/NewsflashScreenTests.cs
@@ -0,0 +1,33 @@
+﻿using Dictator.ConsoleInterface.Events;
+using Dictator.ConsoleInterface;
+
+namespace Dictator.ConsoleInterfaces.Tests.Events;
+
+[TestFixture]
+public class NewsflashScreenTests
+{
+    private NewsflashScreen _newsflashScreen;
+    private Mock<IConsoleService> _consoleServiceMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _consoleServiceMock = new Mock<IConsoleService>();
+        _newsflashScreen = new NewsflashScreen(_consoleServiceMock.Object);
+    }
+
+    [Test]
+    public void Show_ClearsConsoleAndWritesHeadline()
+    {
+        // Arrange
+        string headline = "Breaking News: Dictatorship Takes Control";
+
+        // Act
+        _newsflashScreen.Show(headline);
+
+        // Assert
+        _consoleServiceMock.Verify(cs => cs.Clear(ConsoleColor.Gray), Times.Once);
+        _consoleServiceMock.Verify(cs => cs.WriteAt(1, 10, "NEWSFLASH"), Times.Once);
+        _consoleServiceMock.Verify(cs => cs.WriteAt(1, 14, headline), Times.Once);
+    }
+}
